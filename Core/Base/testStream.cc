4e786077793cb8525d0ad37d35efa3bcc171c724
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-04-23 08:12:47 +0000

    Fix tests.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@37 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Base/testStream.cc
+++ Core/Base/testStream.cc
@@ -1,4 +1,9 @@
-/////////////////////////////////////////////////
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 //! rcsid="$Id$"
 
 // Do a quick test of stream functionality.  We don't
@@ -9,7 +14,7 @@
 #include "Ravl/Types.hh"
 #include "Ravl/BinStream.hh"
 #include "Ravl/TFVector.hh"
-
+#include "Ravl/String.hh"
 #include <unistd.h>
 
 using namespace RavlN;
@@ -17,7 +22,7 @@ using namespace RavlN;
 int SimpleTest();
 int VectorTest();
 
-StringC testFile = "/tmp/testStream" + StringC(getpid());
+StringC testFile = "/tmp/testStream" + StringC((IntT) getpid());
 
 int main() {
   int errLine;
