ea5ea366fdd3315d0454fd40c335e6954d3fe52b
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-04-26 07:49:19 +0000

    Doc fix.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@60 7226f50c-d813-0410-99bf-979b288ab488

--- Math/LinearAlgebra/FixedSize/FMatrix.hh
+++ Math/LinearAlgebra/FixedSize/FMatrix.hh
@@ -8,7 +8,7 @@
 #define RAVLFMATRIX_HEADER 1
 //////////////////////////////////////////////////
 //! rcsid=$Id$
-//! file="Ravl/Math/Geometry/Euclidean/TFVector.hh"
+//! file="Ravl/Math/Geometry/Euclidean/FMatrx.hh"
 //! lib=RavlMath
 //! userlevel=Develop
 //! author="Charles Galambos"
