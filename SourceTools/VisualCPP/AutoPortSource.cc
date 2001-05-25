b4bfc9b760463559e27d9279c719c9519bb71bbf
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-05-25 10:54:33 +0000

    Update licence.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@131 7226f50c-d813-0410-99bf-979b288ab488

--- SourceTools/VisualCPP/AutoPortSource.cc
+++ SourceTools/VisualCPP/AutoPortSource.cc
@@ -1,3 +1,9 @@
+// This file is part of VCPP, Visual C++ porting tool for QMake projects 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU General 
+// Public License (GPL). See the gpl.licence file for details or
+// see http://www.gnu.org/copyleft/gpl.html
+// file-header-ends-here
 ///////////////////////////////////////////////////
 //! rcsid="$Id$"
 
