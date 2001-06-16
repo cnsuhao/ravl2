02e5c5a7ca4a4b329ff9836c97ba2ef7b21d037a
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-06-16 19:00:35 +0000

    Changed to use FIndex.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@211 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Base/Index3d.hh
+++ Core/Base/Index3d.hh
@@ -15,7 +15,7 @@
 //! date="2/8/1994"
 //! rcsid="$Id$"
 
-#include "Ravl/Index.hh" 
+#include "Ravl/FIndex.hh" 
 #include "Ravl/Assert.hh"
 
 //====================================================================
@@ -28,7 +28,9 @@ namespace RavlN {
   // as a three dimensional index of three dimensional arrays. <p>
   // SMALL OBJECT.
   
-  class Index3dC {
+  class Index3dC 
+    : public FIndexC<3>
+  {
   public:
     
     // Constructor, assigment, copies, and destructor
@@ -48,12 +50,6 @@ namespace RavlN {
     
     inline Index3dC(const Index3dC & index);
     // Copy constructor.
-    
-    inline bool operator==(const Index3dC & index) const;
-    // Returns TRUE if indexes have the same values.
-    
-    inline bool operator!=(const Index3dC & index) const;
-    // Returns TRUE if indexes have different values.
     
     inline void Set(IndexC i, IndexC j, IndexC k);
     // Sets this index to be <i,j,k>.
@@ -80,14 +76,7 @@ namespace RavlN {
     // Access to the second index.
     
     inline IndexC & K();
-    // Access to the third index.
-    
-    inline IndexC operator[](IndexC i) const;
-    // Returns the i-th index.
-    
-    inline IndexC & operator[](IndexC i);
-    // Access to the i-th index.
-    
+    // Access to the third index.    
     
     // Access to the neighbouring 3D indexes
     // -------------------------------------
@@ -99,7 +88,7 @@ namespace RavlN {
     // Shifts the index to the left.
     
     inline Index3dC & Up();
-  // Shifts the index to the up.
+    // Shifts the index to the up.
     
     inline Index3dC & Down();
     // Shifts the index to the down.
@@ -116,69 +105,10 @@ namespace RavlN {
     inline Index3dC DownN() const;
     // Returns the index of the down neighbour.
     
-    // Arithmetic operations
-    // ---------------------
-    
-    inline const Index3dC & operator+=(const Index3dC & ind);
-    // Adds 'ind' to this index'.
-    
-    inline const Index3dC & operator-=(const Index3dC & ind);
-    // Subtract index 'ind' from this index.
-    
-    inline const Index3dC & operator*=(const Index3dC & ind);
-    // Multiplies this index by index 'ind' item by item.
-    
-    inline const Index3dC & operator/=(const Index3dC & ind);
-    // Divides this index by index 'ind' item by item.
-    
-    inline Index3dC operator+(const Index3dC & ind) const;
-    // Adds this index and 'ind'.
-  
-    inline Index3dC operator-(const Index3dC & ind) const;
-    // Subtract index 'ind' from this index.
-    
-    inline Index3dC operator*(const Index3dC & ind) const;
-    // Returns this index multiplied by index 'ind' item by item.
-    
-    inline Index3dC operator/(const Index3dC & ind) const;
-    // Returns this index divided by index 'ind' item by item.
-    
-    inline Index3dC operator*(const IntT alpha) const;
-    // Multiplies this index by number 'alpha'.
-    
-    inline Index3dC operator*(const RealT alpha) const;
-    // Multiplies this index by real number 'alpha'.
-    
-    inline Index3dC operator/(const IntT alpha) const;
-    // Divides this index by number 'alpha'.
-    
-    inline Index3dC operator/(const RealT alpha) const;
-    // Divides this index by real number 'alpha'.
-    
     // Special member functions
     // ------------------------
-    
-    inline UIntT Hash(void) const;
-    // Generates a randomised hash value for this index.
-    
-    inline SizeT Dim(void) const;
-    // Returns a number of dimensions indexed.
-    
-  private:
-    
-    // Object representation
-    // ---------------------
-    
-    enum {dim = 3};  // number of indexes     
-    IndexC ii[dim];  // the indexes
+        
   };
-  
-  ostream & operator<<(ostream & outS, const Index3dC & index);
-  // Sends the index into the output stream.
-  
-  istream & operator>>(istream & inS, Index3dC & index);
-  // Reads the index values from the input stream.
-  
   
   //====================================================================
   //======= Index3dC ===================================================
@@ -189,60 +119,35 @@ namespace RavlN {
   Index3dC::Index3dC(SizeT dim) 
   { RavlAssert(dim == 3); }
   
-  inline 
-  SizeT 
-  Index3dC::Dim(void) const
-  { return dim; }
-  
   inline
   IndexC
   Index3dC::I() const
-  { return ii[0]; }
+  { return (*this)[0]; }
   
   inline
   IndexC
   Index3dC::J() const
-  { return ii[1]; }
+  { return (*this)[1]; }
   
   inline
   IndexC
   Index3dC::K() const
-  { return ii[2]; }
+  { return (*this)[2]; }
   
   inline
   IndexC &
   Index3dC::I()
-  { return ii[0]; }
+  { return (*this)[0]; }
 
   inline
   IndexC &
   Index3dC::J()
-  { return ii[1]; }
+  { return (*this)[1]; }
 
   inline
   IndexC &
   Index3dC::K()
-  { return ii[2]; }
-
-  inline 
-  IndexC 
-  Index3dC::operator[](IndexC i) const {
-#if RAVL_CHECK
-    if (i<0 ||  i >= ((int) dim)) 
-      IssueError(__FILE__,__LINE__,"bad index value %d ",i.V());
-#endif
-    return ii[i.V()];
-  }
-  
-  inline 
-  IndexC & 
-  Index3dC::operator[](IndexC i) {
-#if RAVL_CHECK
-    if (i<0 || i >= ((int) dim)) 
-      IssueError(__FILE__,__LINE__,"bad index value %d ",i.V());
-#endif
-    return ii[i.V()];
-  }
+  { return (*this)[2]; }
   
   inline
   void
@@ -268,16 +173,6 @@ namespace RavlN {
   inline
   Index3dC::Index3dC(const Index3dC & index)
   { Set(index.I(), index.J(), index.K()); }
-  
-  inline
-  bool
-  Index3dC::operator==(const Index3dC & index) const 
-  { return (index.I()==I()) && (index.J()==J()) && (index.K()==K()); }
-  
-  inline
-  bool
-  Index3dC::operator!=(const Index3dC & index) const
-  { return (index.I()!=I()) || (index.J()!=J()) || (index.K()!=K()); }
   
   inline
   void
@@ -344,97 +239,6 @@ namespace RavlN {
     return id;
   }
   
-  inline 
-  const Index3dC &
-  Index3dC::operator+=(const Index3dC & ind) {
-    I() += ind.I();
-    J() += ind.J();
-    K() += ind.K();
-    return *this;
-  }
-  
-  inline 
-  const Index3dC &
-  Index3dC::operator-=(const Index3dC & ind) {
-    I() -= ind.I();
-    J() -= ind.J();
-    K() -= ind.K();
-    return *this;
-  }
-  
-  inline 
-  const Index3dC &
-  Index3dC::operator*=(const Index3dC & ind) {
-    I() *= ind.I();
-    J() *= ind.J();
-    K() *= ind.K();
-    return *this;
-  }
-  
-  inline 
-  const Index3dC &
-  Index3dC::operator/=(const Index3dC & ind){
-    I() /= ind.I();
-    J() /= ind.J();
-    K() /= ind.K();
-    return *this;
-  }
-  
-  inline 
-  Index3dC 
-  Index3dC::operator+(const Index3dC & ind) const 
-  { return Index3dC(I()+ind.I(), J()+ind.J(), K()+ind.K()); }
-
-  inline 
-  Index3dC 
-  Index3dC::operator-(const Index3dC & ind) const 
-  { return Index3dC(I()-ind.I(), J()-ind.J(), K()-ind.K()); }
-
-  inline 
-  Index3dC 
-  Index3dC::operator*(const Index3dC & ind) const
-  { return Index3dC(I()*ind.I(), J()*ind.J(), K()*ind.K()); }
-
-  inline 
-  Index3dC 
-  Index3dC::operator/(const Index3dC & ind) const
-  { return Index3dC(I()/ind.I(), J()/ind.J(), K()/ind.K()); }
-
-  inline 
-  Index3dC 
-  Index3dC::operator*(const IntT alpha) const
-  { return Index3dC(I()*alpha, J()*alpha, K()*alpha); }
-
-  inline 
-  Index3dC 
-  Index3dC::operator*(const RealT alpha) const {
-    return Index3dC((IndexC)(I()*alpha),
-		    (IndexC)(J()*alpha),
-		    (IndexC)(K()*alpha));
-  }
-  
-  inline 
-  Index3dC 
-  Index3dC::operator/(const IntT alpha) const
-  { return Index3dC(I()/alpha, J()/alpha, K()/alpha); }
-
-  inline 
-  Index3dC 
-  Index3dC::operator/(const RealT alpha) const
-  { return Index3dC(I()/alpha, J()/alpha, K()/alpha); }
-
-  inline 
-  UIntT
-  Index3dC::Hash(void) const  { 
-    UIntT HVal = ii[0].V();
-    // The compiler should unroll this.
-    for(IndexC i = 1;i < 3;i++) { 
-      HVal += ((UIntT) ii[i.V()].V());
-      HVal ^= ii[i.V()].V() << (i.V() * (sizeof(IndexC) * 2));
-    }
-    return HVal;
-  }
-
 }  
 #endif
   
