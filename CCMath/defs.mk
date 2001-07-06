623b3b3a15177bb580e6f415297cf1a324309cf8
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-07-06 11:56:41 +0000

    Added parts of CCMATH
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@240 7226f50c-d813-0410-99bf-979b288ab488

---
+++ CCMath/defs.mk
@@ -0,0 +1,19 @@
+# Automatically generated make file
+
+PACKAGE=ccmath
+
+NESTED =  matrix.r 
+# intg.r geom.r cfit.r roots.r fft.r simu.r statf.r sfunc.r sort.r  tseries complex util xarm \
+
+
+#tmp.r benchmk.r 
+
+SOURCES = 
+
+HEADERS = ccmath.h 
+
+MAINS = 
+#byteord.c 
+
+EHT = 
+
