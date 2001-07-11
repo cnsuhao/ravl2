1ffd6df33b58acc018f8fb7c7717d57354a07d0a
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-07-11 14:00:18 +0000

    Doc updates.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@265 7226f50c-d813-0410-99bf-979b288ab488

--- PatternRec/DataSet/DataSet1.hh
+++ PatternRec/DataSet/DataSet1.hh
@@ -1,5 +1,12 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 #ifndef RAVLDATASET1_HEADER
 #define RAVLDATASET1_HEADER 1
+//! rcsid="$Id$"
 
 #include"Ravl/PatternRec/Sample.hh"
 #include"Ravl/Vector.hh"
