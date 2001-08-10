6fe545bd8c4848b9012518bbfe2b492e49a5d1b9
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-08-10 13:39:11 +0000

    Minor fix to include guard.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@330 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Base/RCAbstract.hh
+++ Core/Base/RCAbstract.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef RCABSTRACT_HEADER
-#define RCABSTRACT_HEADER 1
+#ifndef RAVLRCABSTRACT_HEADER
+#define RAVLRCABSTRACT_HEADER 1
 //////////////////////////////////////////////////
 //! userlevel=Normal
 //! author="Charles Galambos"
