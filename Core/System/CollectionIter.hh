6d5ae788acc66f0954398c543ef9b2a54efc0787
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-08-10 15:04:03 +0000

    Doc upate.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@332 7226f50c-d813-0410-99bf-979b288ab488

--- Core/System/CollectionIter.hh
+++ Core/System/CollectionIter.hh
@@ -1,8 +1,15 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 #ifndef RAVLCOLLECTIONITER_HEADER
 #define RAVLCOLLECTIONITER_HEADER 1
 //////////////////////////////////////////////////////////////
 //! rcsid="$Id$"
 //! docentry="Ravl.Core.Misc"
+//! lib=RavlCore
 
 #include "Ravl/Collection.hh"
 #include "Ravl/SArr1Iter.hh"
