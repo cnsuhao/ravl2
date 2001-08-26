6a31adee1d84a988b4b6498cdc194e822c8fc8d1
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-08-26 18:46:33 +0000

    Added AVL and simple Binary tree's
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@336 7226f50c-d813-0410-99bf-979b288ab488

---
+++ Core/Container/Trees/defs.mk
@@ -0,0 +1,18 @@
+# This file is part of RAVL, Recognition And Vision Library 
+# Copyright (C) 2001, University of Surrey
+# This code may be redistributed under the terms of the GNU Lesser
+# General Public License (LGPL). See the lgpl.licence file for details or
+# see http://www.gnu.org/copyleft/lesser.html
+# file-header-ends-here
+# $Id$
+#! rcsid="$Id$"
+
+PACKAGE=Ravl
+
+HEADERS=BinaryTree.hh AVLTree.hh BinaryTreeUpIter.hh BinaryTreeDownIter.hh
+
+PLIB=RavlCore
+
+TESTEXES=testBinaryTree.cc
+
+EHT=Ravl.Core.Trees.html
