c4877ca2bb6f7ab186ab1eca3187ee4d042ee1d1
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-09-17 12:48:16 +0000

    Doc updates and new constructor.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@388 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Base/IndexRange2d.hh
+++ Core/Base/IndexRange2d.hh
@@ -52,6 +52,15 @@ namespace RavlN {
       {}
     //: Create an 2d range from corner points.
 
+    IndexRange2dC(const Index2dC &center,SizeT size)
+      : rows(center.Row()-size, center.Row()+size), 
+      cols(center.Col()-size,center.Col()+size)
+      {}
+    //: Create an 2d range from a center point and a size.
+    // Size is the distance from the center to the edge, so
+    // a size of 0 gives a single pixel, and a size of 1 generates
+    // a 3x3 square.
+    
     IndexRange2dC(IndexC minRow, IndexC maxRow,
 		    IndexC minCol, IndexC maxCol)
       : rows(minRow, maxRow), 
@@ -61,47 +70,47 @@ namespace RavlN {
     
     inline Index2dC Origin() const
       { return Index2dC(rows.Min(),cols.Min()); }
-    // Returns the top-left index of the rectangle.
+    //: Returns the top-left index of the rectangle.
     
     inline Index2dC  End() const
       { return Index2dC(rows.Max(),cols.Max()); }
-    // Returns the bottom-right index of the rectangle.
+    //: Returns the bottom-right index of the rectangle.
     
     inline Index2dC Center() const
       { return Index2dC(rows.Center(),cols.Center()); }
-    // Returns the index which is in the middle of the rectangle
+    //: Returns the index which is in the middle of the rectangle
     
     inline IndexC TRow() const
       { return rows.Min(); }
-    // Returns the top row index.
+    //: Returns the top row index.
     
     inline IndexC LCol() const
       { return cols.Min(); }
-    // Returns the left side column index.
+    //: Returns the left side column index.
     
     inline IndexC BRow() const
       { return rows.Max(); }
-    // Returns the bottom row index.
+    //: Returns the bottom row index.
     
     inline IndexC RCol() const
       { return cols.Max(); }
-    // Returns the right side column index.
+    //: Returns the right side column index.
 
     inline IndexC &TRow()
       { return rows.Min(); }
-    // Returns the top row index.
+    //: Returns the top row index.
     
     inline IndexC &LCol()
       { return cols.Min(); }
-    // Returns the left side column index.
+    //: Returns the left side column index.
     
     inline IndexC &BRow()
       { return rows.Max(); }
-    // Returns the bottom row index.
+    //: Returns the bottom row index.
     
     inline IndexC &RCol()
       { return cols.Max(); }
-    // Returns the right side column index.
+    //: Returns the right side column index.
     
     inline SizeT Rows() const
       { return rows.Size(); }
@@ -113,24 +122,24 @@ namespace RavlN {
 
     inline SizeT Area() const
       { return (SizeT) Rows() * Cols(); }
-    // Returns the area of the image rectangle expressed in number of indexs.
+    //: Returns the area of the image rectangle expressed in number of indexs.
     
     inline IndexRange2dC Dilate() const 
-      { return IndexRange2dC(rows.Expand(1),cols.Expand(1)); }
+    { return IndexRange2dC(rows.Expand(1),cols.Expand(1)); }
     //: Returns a new rectangle one index larger on each side.
     
     inline IndexRange2dC Erode() const
       { return IndexRange2dC(rows.Shrink(1),cols.Shrink(1)); }
-    // Removes one index layer on each side.
+    //: Returns an rectangle with each side 1 index closer to the center.
     
     inline IndexRange2dC Expand(IndexC n) const
       { return IndexRange2dC(rows.Expand(n),cols.Expand(n)); }
-    // Expands the rectangle by adding 'n' indexs on each side.
+    //: Returns an rectangle expanded by 'n' indexs on each side.
     
     inline IndexRange2dC Shrink(IndexC n) const
       { return IndexRange2dC(rows.Shrink(n),cols.Shrink(n)); }
-    // Returns a new rectangle which has layer of the width of 'n' indexs
-    // removed.
+    //: Returns a new rectangle which has layer of the width of 'n' indexs
+    //: removed.
     
     inline IndexRange2dC & ClipBy(const IndexRange2dC & r) {
       Range1().ClipBy(r.Range1());
@@ -138,16 +147,20 @@ namespace RavlN {
       return *this;
     }
     //: This index range is clipped to contain at most the index range 'r'.
-
+    
     inline bool Contains(const IndexRange2dC & oth) const
-      { return Range1().Contains(oth.Range1()) && Range2().Contains(oth.Range2()); }
-    // Returns true if this range contains the subrange 'oth'.
+    { return Range1().Contains(oth.Range1()) && Range2().Contains(oth.Range2()); }
+    //: Returns true if this range contains the subrange 'oth'.
+    
+    inline bool Contains(const Index2dC & oth) const
+    { return Range1().Contains(oth.Row()) && Range2().Contains(oth.Col()); }
+    //: Returns true if this range contains the subrange 'oth'.
     
     inline const IndexRange2dC & operator+=(const Index2dC & offset);
-    // Shifts the rectangle to the new position.
+    //: Shifts the rectangle to the new position.
     
     inline const IndexRange2dC & operator-=(const Index2dC & offset);
-    // Shifts the rectangle to the new position.
+    //: Shifts the rectangle to the new position.
     
     inline const IndexRangeC & RowRange() const
       { return rows; }
@@ -182,13 +195,14 @@ namespace RavlN {
     //: Access col range.
     
     inline void Involve(const Index2dC & index);
-    // Checks and changes, if necessary, the 2 dimensional range
+    //: Ensures this rectangle contains given index.
+    // This method checks and changes, if necessary, the 2 dimensional range
     // to contain the 'index'.
     
     inline 
     bool IsValid() const 
       { return rows.IsValid() && cols.IsValid(); }
-    // Returns TRUE if this rectangle contains at least one index.
+    //: Returns TRUE if this rectangle contains at least one index.
 
     bool operator==(const IndexRange2dC &oth) const
       { return oth.Range1() == Range1() && oth.Range2() == Range2(); }
