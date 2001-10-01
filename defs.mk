7172c3dca4bef2d85e62e9b05e2d26dd5c3fc619
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-10-01 10:17:26 +0000

    Added 3D module
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@457 7226f50c-d813-0410-99bf-979b288ab488

--- defs.mk
+++ defs.mk
@@ -10,7 +10,7 @@
 
 PACKAGE=Ravl
 
-NESTED=QMake.r Core.r OS.r CCMath.r Math.r PatternRec.r Image.r Logic.r \
+NESTED=QMake.r Core.r OS.r CCMath.r Math.r PatternRec.r Image.r 3D.r Logic.r \
   SourceTools.r GUI.r Applications.r
 
 LOCAL_FILES=install lgpl.license gpl.license README
