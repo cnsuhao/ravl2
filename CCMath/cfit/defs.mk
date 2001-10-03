1432eb5831b6d7f58c7e358e201b41a80dd84bf7
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-10-03 09:20:38 +0000

    Original cfit code.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@461 7226f50c-d813-0410-99bf-979b288ab488

---
+++ CCMath/cfit/defs.mk
@@ -0,0 +1,14 @@
+# Automatically generated make file
+# $Id$
+
+PACKAGE=ccmath
+
+SOURCES = chcof.c csfit.c csplp.c evpsq.c fitval.c gnlsq.c plsq.c psqcf.c \
+ qrbdbv.c qrvar.c sv2lsq.c tnsfit.c chpade.c cspl.c  dcspl.c evpsqv.c ftch.c \
+ lsqsv.c  pplsq.c  psqvar.c  qrlsq.c   seqlsq.c	svdlsq.c
+
+PLIB = ccmath
+
+LIBDEPS=ccmath_cfit.def
+
+USESLIBS=ccmath
