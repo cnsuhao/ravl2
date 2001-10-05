1da36f197efb4e6c88ec0f1fba3504cb063e06c9
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-10-05 13:10:08 +0000

    Added cfit directory
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@466 7226f50c-d813-0410-99bf-979b288ab488

--- CCMath/defs.mk
+++ CCMath/defs.mk
@@ -2,7 +2,7 @@
 
 PACKAGE=ccmath
 
-NESTED =  matrix.r fft.r
+NESTED =  matrix.r fft.r cfit.r
 # intg.r geom.r cfit.r roots.r fft.r simu.r statf.r sfunc.r sort.r  tseries complex util xarm \
 #tmp.r benchmk.r 
 
