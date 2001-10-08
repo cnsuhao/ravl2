a26f658000705137c1bfc011ee89804987f51f4f
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-10-08 17:17:13 +0000

    Doc updates.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@492 7226f50c-d813-0410-99bf-979b288ab488

--- Core/System/XMLTree.hh
+++ Core/System/XMLTree.hh
@@ -1,8 +1,17 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 #ifndef RAVL_XMLTREE_HEADER
 #define RAVL_XMLTREE_HEADER 1
 ////////////////////////////////////////////////////////////////
 //! author="Charles Galambos"
+//! docentry="Ravl.Core.XML"
 //! example=exXMLTree.cc
+//! rcsid="$Id$"
+//! lib=RavlCore
 
 #include "Ravl/RefCounter.hh"
 #include "Ravl/XMLStream.hh"
