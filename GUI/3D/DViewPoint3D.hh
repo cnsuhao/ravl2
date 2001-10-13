360f5acd907013d5af5a06accc1fb3cd17a32039
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-10-13 14:51:23 +0000

    Fix include guard.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@513 7226f50c-d813-0410-99bf-979b288ab488

--- GUI/3D/DViewPoint3D.hh
+++ GUI/3D/DViewPoint3D.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef DVIEWPOINT_HEADER
-#define DVIEWPOINT_HEADER 1
+#ifndef RAVL_DVIEWPOINT_HEADER
+#define RAVL_DVIEWPOINT_HEADER 1
 ////////////////////////////////////////////
 //! docentry="Ravl.GUI.3D"
 //! rcsid="$Id$"
