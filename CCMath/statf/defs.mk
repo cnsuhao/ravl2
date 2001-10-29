0e39b25d95e7921255271cf78db273ddea9150fa
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-10-29 14:27:36 +0000

    Added statistic functions.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@546 7226f50c-d813-0410-99bf-979b288ab488

---
+++ CCMath/statf/defs.mk
@@ -0,0 +1,14 @@
+# Automatically generated make file
+# $Id$
+
+LICENSE=own
+
+PACKAGE=ccmath
+
+SOURCES = gaml.c pctb.c pctbn.c pctg.c pctgn.c pctn.c qbeta.c qbnc.c qgama.c qgnc.c qnorm.c
+
+PLIB = ccmath
+
+LIBDEPS=ccmath_statf.def
+
+USESLIBS=ccmath
