e986f460f3730f433efad40dd2d865171dee1dea
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-11-02 17:17:02 +0000

    Added statf directory.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@560 7226f50c-d813-0410-99bf-979b288ab488

--- CCMath/defs.mk
+++ CCMath/defs.mk
@@ -2,9 +2,7 @@
 
 PACKAGE=ccmath
 
-NESTED =  matrix.r fft.r cfit.r
-# intg.r geom.r cfit.r roots.r fft.r simu.r statf.r sfunc.r sort.r  tseries complex util xarm \
-#tmp.r benchmk.r 
+NESTED =  matrix.r fft.r cfit.r statf.r
 
 HEADERS = ccmath.h 
 
