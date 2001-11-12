d1cd2b3a63cc90e5276e5b1388ca15614086b0c2
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2001-11-12 15:49:03 +0000

    Doc updates.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@577 7226f50c-d813-0410-99bf-979b288ab488

--- OS/Misc/SerialIO.hh
+++ OS/Misc/SerialIO.hh
@@ -1,3 +1,9 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 #ifndef RAVL_SERIALIO_HEADER
 #define RAVL_SERIALIO_HEADER 1
 /////////////////////////////////////////////////////////////
@@ -6,6 +12,7 @@
 //! author="Fangxiang Cheng"
 //! docentry="Ravl.OS.Hardware"
 //! rcsid="$Id$"
+//! lib=RavlOS
 
 #include "Ravl/Stream.hh"
 
