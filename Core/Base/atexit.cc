0d12f20f658a13e770fd32d413f920507cf08df6
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-01-07 11:25:16 +0000

    Add atexit.cc.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@668 7226f50c-d813-0410-99bf-979b288ab488

---
+++ Core/Base/atexit.cc
@@ -0,0 +1,54 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+#ifdef __sol2__
+#include <stdlib.h>
+#include "Ravl/Atomic.hh"
+//! rcsid="$Id$"
+//! lib=RavlCore
+
+#define TABLESIZE 1024
+
+// This file provides a fix for implementations of libc with
+// a limited number of calls to atexit.
+
+extern "C" {
+  typedef void (*funcptrT)(void);
+  
+  
+  static funcptrT ftable[TABLESIZE];
+  static atomic_t reg = ATOMIC_INIT(-1);
+  
+  int atexit(void (*fptr)(void)) {
+    int x = atomic_inc_return(&reg);
+    if(x >= TABLESIZE) {
+      atomic_dec(&reg);
+      return -1;
+    }
+    ftable[x] = fptr;
+    return 0;
+  }
+  
+  /* I'm assuming by the time this is called we're back to being single threaded. */
+  
+  void _exithandle(void) {
+    int c;
+    /* It is just possible that an exit function could inadvernantly call atexit,
+       and register another function, so be carefull.... */
+    while((c = atomic_read(&reg)) >= 0) {
+      if(ftable[c] != 0)
+	ftable[c]();
+      atomic_dec(&reg);
+    }
+  }
+  
+};
+#else
+namespace RavlN {
+  int foobar_or_some_such_silly_name;
+}
+#endif
+
