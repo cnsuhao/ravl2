010a135b3e8fb75a5fddfb66a069b58bdf72c34d
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-02-07 17:57:02 +0000

    Fix include gards.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@777 7226f50c-d813-0410-99bf-979b288ab488

--- Core/IO/Pipes.hh
+++ Core/IO/Pipes.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef DPPIPE_HEADER
-#define DPPIPE_HEADER 1
+#ifndef RAVL_DPPIPE_HEADER
+#define RAVL_DPPIPE_HEADER 1
 ////////////////////////////////////////////////
 //! rcsid="$Id$"
 //! file="Ravl/Core/IO/Pipes.hh"
