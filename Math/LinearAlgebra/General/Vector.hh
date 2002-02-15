b9cdab0b99e1eeca03c2d55d3d5da4b21c849d47
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-02-15 21:58:50 +0000

    Added some constructors to make generating small vectors easier.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@804 7226f50c-d813-0410-99bf-979b288ab488

--- Math/LinearAlgebra/General/Vector.hh
+++ Math/LinearAlgebra/General/Vector.hh
@@ -33,17 +33,69 @@ namespace RavlN {
     explicit VectorC(UIntT size)
       : TVectorC<RealT>(size)
       {}
-    //: Default constructor.
-
+    //: Constructor.
+    // Create a vector of 'size' elements
+    
     VectorC(const TVectorC<RealT> &oth)
       : TVectorC<RealT>(oth)
       {}
     //: Base class constructor.
-
+    
     VectorC(const SArray1dC<RealT> &oth)
       : TVectorC<RealT>(oth)
       {}
     //: Base class constructor.
+    
+    VectorC(RealT v1,RealT v2)
+      : TVectorC<RealT>(2)
+    {
+      (*this)[0] = v1;
+      (*this)[1] = v2;
+    }
+    //: Create a vector with two real values.
+
+    VectorC(RealT v1,RealT v2,RealT v3)
+      : TVectorC<RealT>(2)
+    {
+      (*this)[0] = v1;
+      (*this)[1] = v2;
+      (*this)[2] = v3;
+    }
+    //: Create a vector with three real values.
+
+    VectorC(RealT v1,RealT v2,RealT v3,RealT v4)
+      : TVectorC<RealT>(3)
+    {
+      (*this)[0] = v1;
+      (*this)[1] = v2;
+      (*this)[2] = v3;
+      (*this)[3] = v4;
+    }
+    //: Create a vector with four real values.
+    
+    VectorC(RealT v1,RealT v2,RealT v3,RealT v4,RealT v5)
+      : TVectorC<RealT>(3)
+    {
+      (*this)[0] = v1;
+      (*this)[1] = v2;
+      (*this)[2] = v3;
+      (*this)[3] = v4;
+      (*this)[4] = v5;
+    }
+    //: Create a vector with five real values.
+
+    VectorC(RealT v1,RealT v2,RealT v3,RealT v4,RealT v5,RealT v6)
+      : TVectorC<RealT>(3)
+    {
+      (*this)[0] = v1;
+      (*this)[1] = v2;
+      (*this)[2] = v3;
+      (*this)[3] = v4;
+      (*this)[4] = v5;
+      (*this)[5] = v6;
+    }
+    //: Create a vector with six real values.
+
     
     bool IsReal() const;
     //: Test if vector only contains real values.
