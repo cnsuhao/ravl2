80fdbea312e8f774cf44bf07e7ef2cbf1a128db0
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-02-15 16:01:47 +0000

    Doc update.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@800 7226f50c-d813-0410-99bf-979b288ab488

--- 3D/Carve3D/VoxelSurfacePoints.hh
+++ 3D/Carve3D/VoxelSurfacePoints.hh
@@ -1,8 +1,16 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 #ifndef RAVL3D_VOXELSURFACEPOINTS_HEADER
 #define RAVL3D_VOXELSURFACEPOINTS_HEADER 1
 //! docentry="Ravl.3D.Carve3D"
 //! author="Joel Mitchelson"
 //! userlevel=Normal
+//! rcsid="$Id$"
+//! lib=RavlCarve3D
 
 #include "Ravl/3D/VoxelSet.hh"
 #include "Ravl/3D/PointSet.hh"
