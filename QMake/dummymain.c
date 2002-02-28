bfcb408432582131a226a0f53d021beaf1521748
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-02-28 11:35:35 +0000

    Fix warning introduced by annotate.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@853 7226f50c-d813-0410-99bf-979b288ab488

--- QMake/dummymain.c
+++ QMake/dummymain.c
@@ -1,5 +1,4 @@
 /* This file is part of QMake, Quick Make System 
-/*! file="Ravl/QMake/dummymain.c" */
 // Copyright (C) 2001, University of Surrey
 // This code may be redistributed under the terms of the GNU General 
 // Public License (GPL). See the gpl.licence file for details or
@@ -8,6 +7,7 @@
 /* Dummy main to force generation of templates. */
 /* $Id$ */
 /*! rcsid="$Id$" */
+/*! file="Ravl/QMake/dummymain.c" */
 #include <stdio.h>
 int main() {
   printf("Dummy main. \n");
