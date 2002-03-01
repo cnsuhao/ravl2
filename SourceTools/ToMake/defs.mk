0298eab6b69bd77e7cdc622d2ed86ec1a10c6ef9
kier <kier@7226f50c-d813-0410-99bf-979b288ab488>
2002-03-01 13:57:18 +0000

    tools to port to make
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@861 7226f50c-d813-0410-99bf-979b288ab488

---
+++ SourceTools/ToMake/defs.mk
@@ -0,0 +1,33 @@
+# This file is part of VCPP, Visual C++ porting tool for QMake projects 
+# Copyright (C) 2001, University of Surrey
+# This code may be redistributed under the terms of the GNU General 
+# Public License (GPL). See the gpl.licence file for details or
+# see http://www.gnu.org/copyleft/gpl.html
+# file-header-ends-here
+# $Id$
+#! rcsid="$Id$"
+#! file="Ravl/SourceTools/VisualCPP/defs.mk"
+
+PACKAGENAME= TOMAKE
+
+DESCRIPTION = Porting to the normal make system
+
+LICENSE = GPL
+
+PACKAGE=Ravl/SourceTools
+
+HEADERS= 
+
+SOURCES =
+
+MAINS = tomake.cc
+
+USESLIBS = RavlOS RavlSourceTools
+
+EHT = 
+
+# Deal with template files.
+AUXFILES = Makefile.tmpl
+
+AUXDIR= Tools/AutoPort
+
