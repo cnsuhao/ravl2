2822175d470a9bb8d1c4032e7d473c2982ba950b
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-03-21 18:07:11 +0000

    Minor tidy.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@913 7226f50c-d813-0410-99bf-979b288ab488

--- OS/Threads/Posix/Thread.hh
+++ OS/Threads/Posix/Thread.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef RAVLPTHREADSTHREAD_HEADER
-#define RAVLPTHREADSTHREAD_HEADER 1
+#ifndef RAVL_THREADSTHREAD_HEADER
+#define RAVL_THREADSTHREAD_HEADER 1
 //////////////////////////////////////////////////////
 //! rcsid="$Id$"
 //! file="Ravl/OS/Threads/Posix/Thread.hh"
