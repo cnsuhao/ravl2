a5afb73ad17cc13b9a3c50a8fc7dbbbb7cfb9a0d
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-03-22 17:45:25 +0000

    Tidy up, and add some documentation.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@917 7226f50c-d813-0410-99bf-979b288ab488

--- OS/Threads/Posix/Semaphore.hh
+++ OS/Threads/Posix/Semaphore.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef RAVLTHREADSEMAPHORE_HEADER
-#define RAVLTHREADSEMAPHORE_HEADER 1
+#ifndef RAVL_THREADSEMAPHORE_HEADER
+#define RAVL_THREADSEMAPHORE_HEADER 1
 //////////////////////////////////////////////////////////////
 //! userlevel=Normal
 //! author="Charles Galambos"
@@ -19,28 +19,42 @@
 
 namespace RavlN
 {
+  //! userlevel=Normal
   //: Semaphore.
+  // This is a method for communicating between threads. It includes an
+  // atomic counter, which can be incremented by Post(), and decremented
+  // by Wait(). Wait() will only when it can decrement the counter from
+  // a positive number.   One use of this class is to send a signal to
+  // another thread that some processing is ready to be done.
   
   class SemaphoreC {
   public:
     SemaphoreC(int initVal = 1) 
       : count(initVal)
-      {}
+    {}
     //: Constructor.
-
+    // Create a semaphore with an inital count of 'initVal'.
+    
     SemaphoreC(const SemaphoreC &oth) 
       : count(oth.count)
-      {}
+    {}
     //: Copy Constructor.
+    // Create a semaphore with the same count as this
+    // one.  Its not clear how useful this really is.
     
     bool Wait();
     //: Wait for semaphore.
-    // Returns true if semaphore retrieved ok.
+    // Test is semaphore has a count greated than 0,
+    // if it does decrement it and return.
+    // If the count is 0, then wait until it a call
+    // to Post() increments it, then decrement it
+    // and return true.
     
     bool Wait(RealT maxDelay);
     //: Wait for semaphore.
-    // Returns true if semaphore retrieved ok.
-    // False is returned on a time out.
+    // As Wait(), but only wait for semaphore for up to
+    // 'maxDelay' seconds. If the time expires return
+    // false. If the semaphore is recieved then return true.
     
     bool TryWait() {
       if(count == 0)
@@ -55,6 +69,8 @@ namespace RavlN
       return false;
     }
     //: Try and wait for semaphore
+    // Return true if semaphore has been posted, and decrement as it Wait() had 
+    // been called. Otherwise do nothing and return false.
     
     bool Post() {
       cond.Lock();
@@ -64,19 +80,19 @@ namespace RavlN
       return false;
     }
     //: Post a semaphore.
+    // Post a semaphore, increase the semaphore count by 1.
     
-    int Count(void) const 
-      { 
+    int Count(void) const { 
 #if 0
-	int ret; 
-	cond.Lock();
-	ret = count;
-	cond.Unlock();
-	return ret; 
+      int ret; 
+      cond.Lock();
+      ret = count;
+      cond.Unlock();
+      return ret; 
 #else
-	return count; // Is there any point in locking ???
+      return count; // Is there any point in locking ???
 #endif
-      }
+    }
     //: Read semaphore count.
     
   private:
