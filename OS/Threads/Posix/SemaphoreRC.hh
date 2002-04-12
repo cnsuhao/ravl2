37d5922a298a4215a15ccf20aeee02d4e5343369
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-04-12 16:49:43 +0000

    Tidy and get rid on unneeded method VPost().
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@967 7226f50c-d813-0410-99bf-979b288ab488

--- OS/Threads/Posix/SemaphoreRC.hh
+++ OS/Threads/Posix/SemaphoreRC.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef RAVLSEMAPHORERC_HEADER
-#define RAVLSEMAPHORERC_HEADER 1
+#ifndef RAVL_SEMAPHORERC_HEADER
+#define RAVL_SEMAPHORERC_HEADER 1
 ///////////////////////////////////////////////////
 //! rcsid="$Id$"
 //! file="Ravl/OS/Threads/Posix/SemaphoreRC.hh"
@@ -13,50 +13,45 @@
 //! userlevel=Normal
 //! docentry="Ravl.OS.Threads"
 //! author="Charles Galambos"
-//! date="22/11/99"
+//! date="22/11/1999"
 
 #include "Ravl/Threads/Semaphore.hh"
 #include "Ravl/RCWrap.hh"
 
 namespace RavlN
 {
-  //: Semaphore.
+  //! userlevel=Normal
+  //: Refrence counted semaphore.
   
   class SemaphoreRC
     : public RCWrapC<SemaphoreC>
   {
   public:
     SemaphoreRC()
-      {}
+    {}
     //: Default constructor.
     // Creates an invalid handle.
     
     SemaphoreRC(IntT count)
       : RCWrapC<SemaphoreC>(SemaphoreC(count))
-      {}
+    {}
     //: Default constructor.
     // Creates an invalid handle.
 
     bool Wait()
-      { return Data().Wait(); }
+    { return Data().Wait(); }
     //: Wait for semaphore.
     
     bool TryWait() 
-      { return Data().TryWait(); }
+    { return Data().TryWait(); }
     //: Try and wait for semaphore
     
     bool Post() 
-      { return Data().Post(); }
+    { return Data().Post(); }
     //: Post a semaphore.
-    
-    void VPost() 
-      {  Data().Post(); }
-    //: Post a semaphore.
-    // Exactly as post, but with a return type of void
-    // for use with signal functions.
     
     int Count(void) const 
-      { return Data().Count(); }
+    { return Data().Count(); }
     //: Read semaphore count.
     
   private:
