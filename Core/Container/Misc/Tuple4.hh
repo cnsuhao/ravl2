b8a16348f3db0023eb882c255d0dfdf19f6bfdba
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-05-03 13:28:22 +0000

    Bug fix.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@1085 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Container/Misc/Tuple4.hh
+++ Core/Container/Misc/Tuple4.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef RAVLDPTUPLE4_HEADER
-#define RAVLDPTUPLE4_HEADER 1
+#ifndef RAVL_TUPLE4_HEADER
+#define RAVL_TUPLE4_HEADER 1
 /////////////////////////////////////////////////
 //! docentry="Ravl.Core.Tuples"
 //! userlevel=Normal
@@ -13,7 +13,7 @@
 //! file="Ravl/Core/Container/Misc/Tuple4.hh"
 //! lib=RavlCore
 //! author="Charles Galambos"
-//! date="09/06/98"
+//! date="09/06/1998"
 
 #include "Ravl/Types.hh"
 
@@ -26,37 +26,38 @@ namespace RavlN {
   
   class BinIStreamC;
   class BinOStreamC;
-  
+
+  //! userlevel=Normal
   //: Class containing 4 other classes.
   
   template<class T1,class T2,class T3,class T4>
   class Tuple4C {
-    public:
+  public:
     Tuple4C() {}
     //: Default constructor.
     
     Tuple4C(istream &in) 
-      { in >> d1 >> d2 >> d3 >> d4; }
+    { in >> d1 >> d2 >> d3 >> d4; }
     //: Stream constructor.
     
     Tuple4C(BinIStreamC &in) 
-      { in >> d1 >> d2 >> d3 >> d4; }
+    { in >> d1 >> d2 >> d3 >> d4; }
     //: Binary Stream constructor.
     
-    Tuple4C(const T1 &nd1,const T2 &nd2,const T3 &nd3,const T3 &nd4) 
+    Tuple4C(const T1 &nd1,const T2 &nd2,const T3 &nd3,const T4 &nd4) 
       : d1(nd1),
-      d2(nd2),
-      d3(nd3),
-      d4(nd4)
-      {}
+	d2(nd2),
+	d3(nd3),
+	d4(nd4)
+    {}
     //: Constructor.
     
     Tuple4C(const Tuple4C<T1,T2,T3,T4> &oth) 
       : d1(oth.d1),
-      d2(oth.d2),
-      d3(oth.d3),
-      d4(oth.d4)
-      {}
+	d2(oth.d2),
+	d3(oth.d3),
+	d4(oth.d4)
+    {}
     //: Copy constructor.
     
     T1 &Data1() { return d1; }
@@ -92,11 +93,11 @@ namespace RavlN {
     //: Get hash value for tuple.
     
     bool operator==(const Tuple4C<T1,T2,T3,T4> &oth) const
-      { return (oth.d1 == d1) && (oth.d2 == d2) && (oth.d3 == d3)  && (oth.d4 == d4); }
+    { return (oth.d1 == d1) && (oth.d2 == d2) && (oth.d3 == d3)  && (oth.d4 == d4); }
     //: Are tuples equal ?
     
     bool operator!=(const Tuple4C<T1,T2,T3,T4> &oth) const
-      { return  (oth.d1 != d1) || (oth.d2 != d2) || (oth.d3 != d3) || (oth.d4 != d4); }
+    { return  (oth.d1 != d1) || (oth.d2 != d2) || (oth.d3 != d3) || (oth.d4 != d4); }
     //: Are tuples unequal ?
     
   protected:
