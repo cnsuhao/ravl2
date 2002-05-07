310904366a019377665ff4d7b0574f171faccb87
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-05-07 16:46:49 +0000

    Tidy up.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@1096 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Math/Random.hh
+++ Core/Math/Random.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef RavlRandom_HH
-#define RavlRandom_HH
+#ifndef RAVL_RANDOM_HEADER
+#define RAVL_RANDOM_HEADER 1
 ///////////////////////////////////////////////////////////////////////////
 //! rcsid="$Id$"
 //! userlevel=Normal
@@ -25,33 +25,29 @@ extern "C" {
 };
 
 namespace RavlN {  
-
-  //: Seed for random number
+  
   typedef unsigned short RandomSeedT[3];
+  //: Seed for random number
   
   const long int RandomIntMaxValue = 2 ^ 31;
   
-  inline
-  long int RandomInt(RandomSeedT & idum) { 
-    return nrand48(idum);
-  }
-  // Returns an integer between 0 and RandomIntMaxValue.
+  inline long int RandomInt(RandomSeedT & idum) 
+  { return nrand48(idum); }
+  //: Returns an integer between 0 and RandomIntMaxValue.
   
   long int RandomInt();
-  // Returns an integer between 0 and RandomIntMaxValue.
+  //: Returns an integer between 0 and RandomIntMaxValue.
   
-  inline
-  double Random1(RandomSeedT & idum){ 
-    return  erand48(idum); 
-  }
-  // Returns a random number between 0 and 1 with a uniform distribution.
-
+  inline double Random1(RandomSeedT & idum)
+  { return  erand48(idum); }
+  //: Returns a random number between 0 and 1 with a uniform distribution.
+  
   double Random1();
-  // Returns a random number between 0 and 1 with a uniform distribution.
+  //: Returns a random number between 0 and 1 with a uniform distribution.
   
   float  RandomGauss();
-  // Returns a Gaussian distributed zero mean random
-  // number with variance 1.0.
+  //: Returns a Gaussian distributed zero mean random
+  //: number with variance 1.0.
   // RandomGauss is based on Random1.
 
 }
