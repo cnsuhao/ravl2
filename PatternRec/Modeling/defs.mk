52871b50d5037b03c6a3edf318b3b1fe739484ab
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-06-13 18:26:27 +0000

    Doc updates.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@1255 7226f50c-d813-0410-99bf-979b288ab488

--- PatternRec/Modeling/defs.mk
+++ PatternRec/Modeling/defs.mk
@@ -1,5 +1,12 @@
+# This file is part of RAVL, Recognition And Vision Library 
+# Copyright (C) 2002, University of Surrey
+# This code may be redistributed under the terms of the GNU Lesser
+# General Public License (LGPL). See the lgpl.licence file for details or
+# see http://www.gnu.org/copyleft/lesser.html
+# file-header-ends-here
+#! rcsid="$Id$"
 
-DESCRIPTION= Classes for numerical modeling of functions. e.g. for estimating PDFs
+DESCRIPTION= Classes for numerical modeling of functions. 
 
 NESTED=Basic.r
 
