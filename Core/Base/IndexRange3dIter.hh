548e0ae136a1cc5ff20b688f6300e1b929238ffd
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-06-18 11:10:53 +0000

    Annotation update.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@1274 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Base/IndexRange3dIter.hh
+++ Core/Base/IndexRange3dIter.hh
@@ -1,8 +1,15 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2002, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 #ifndef RAVL_INDEXRANGE3DITER_HEADER
 #define RAVL_INDEXRANGE3DITER_HEADER 1
 //! rcsid="$Id$"
 //! author="Charles Galambos"
 //! docentry="Ravl.Core.Indexing"
+//! lib=RavlCore
 
 #include "Ravl/IndexRange3d.hh"
 
