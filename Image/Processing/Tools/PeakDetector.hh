fb89497f871f9a5a847e6d5ea1f9d49b538f64dd
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-07-24 15:31:04 +0000

    Bug fix.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@1448 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Tools/PeakDetector.hh
+++ Image/Processing/Tools/PeakDetector.hh
@@ -14,6 +14,8 @@
 
 #include "Ravl/Array2d.hh"
 #include "Ravl/Index2d.hh"
+#include "Ravl/Point2d.hh"
+#include "Ravl/Vector2d.hh"
 
 namespace RavlImageN {
   using namespace RavlN;
@@ -112,7 +114,7 @@ namespace RavlImageN {
 
 
   template<class DataT>
-  Point2dC LocatePeakSubPixel(const Array2dC<DataT> &img,const Index2dC &pos,RealT pof = 0.25) {
+  Point2dC LocatePeakSubPixel(const Array2dC<DataT> &img,const Index2dC &pos,RealT pof) {
     // apply geometric fitting in image-proportional coordinates.
     if(!img.Frame().Erode().Contains(pos))
       return pos;
@@ -131,6 +133,57 @@ namespace RavlImageN {
     RealT snp = pow((RealT) rt[-1],pof);
     RealT snc = Pow((RealT) rt[ 0],pof);
     RealT snn = Pow((RealT) rt[ 1],pof);
+    
+    // Use least-squares to fit quadratic to local corner strengths.
+    RealT Pxx = (spp - 2.0*spc + spn + scp - 2.0*scc + scn + snp - 2.0*snc + snn)/3.0;
+    RealT Pxy = (spp - spn - snp + snn)/4.0;
+    RealT Pyy = (spp + spc + spn - 2.0*scp - 2.0*scc - 2.0*scn + snp + snc + snn)/3.0;
+    RealT Px = (- spp - scp - snp + spn + scn + snn)/6.0;
+    RealT Py = (- spp - spc - spn + snp + snc + snn)/6.0;
+    RealT det = Pxy*Pxy - Pxx*Pyy;
+    
+    if(det == 0)
+      return Point2dC(pos);
+    
+    // calculate sub-pixel corrections to the corner position.
+    Vector2dC corr((Pyy*Px - Pxy*Py)/det,(Pxx*Py - Pxy*Px)/det);
+    
+    // pull the corrections inside the pixel.
+    if (corr[0] > 0.5) 
+     corr[0]=0.5; 
+    if (corr[0] < -0.5) 
+      corr[0]= -0.5;
+    if (corr[1] > 0.5) 
+      corr[1]=0.5; 
+    if (corr[1] < -0.5) 
+      corr[1]= -0.5;
+    return Point2dC(pos) + corr; 
+  }
+  //: Locate peak with sub-pixel precision.
+  // Fits a quadratic to the peak and works out the center. The position of the
+  // peak is returned. 'img' should contain values surrounding the center of
+  // the peak at 'pos'.
+
+  template<class DataT>
+  Point2dC LocatePeakSubPixel(const Array2dC<DataT> &img,const Index2dC &pos) {
+    // apply geometric fitting in image-proportional coordinates.
+    if(!img.Frame().Erode().Contains(pos))
+      return pos;
+    
+    const DataT *rt = &(img[pos[0]-1][pos[1]]);
+    RealT spp = (RealT) rt[-1];
+    RealT spc = (RealT) rt[ 0];
+    RealT spn = (RealT) rt[ 1];
+    
+    rt = &(img[pos]);
+    RealT scp = (RealT) rt[-1];
+    RealT scc = (RealT) rt[ 0];
+    RealT scn = (RealT) rt[ 1];
+    
+    rt = &(img[pos[0]+1][pos[1]]);
+    RealT snp = (RealT) rt[-1];
+    RealT snc = (RealT) rt[ 0];
+    RealT snn = (RealT) rt[ 1];
     
     // Use least-squares to fit quadratic to local corner strengths.
     RealT Pxx = (spp - 2.0*spc + spn + scp - 2.0*scc + scn + snp - 2.0*snc + snn)/3.0;
