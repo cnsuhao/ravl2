e76dec321437025476cc8663cecbd8c56d4ba8bf
philfm <philfm@7226f50c-d813-0410-99bf-979b288ab488>
2002-08-28 11:08:41 +0000

    Added PDF file to AUXFILES
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@1627 7226f50c-d813-0410-99bf-979b288ab488

--- Math/Optimisation/DocLevenbergMarquardt/defs.mk
+++ Math/Optimisation/DocLevenbergMarquardt/defs.mk
@@ -3,5 +3,31 @@
 
 AUXDIR=share/doc/RAVL/LevenbergMarquardt
 
-AUXFILES= 
+AUXFILES= levmarq/img1.png levmarq/img2.png levmarq/img3.png \
+   levmarq/img4.png levmarq/img5.png levmarq/img6.png levmarq/img7.png \
+   levmarq/img8.png levmarq/img9.png levmarq/img10.png levmarq/img11.png \
+   levmarq/img12.png levmarq/img13.png levmarq/img14.png levmarq/img15.png \
+   levmarq/img16.png levmarq/img17.png levmarq/img18.png levmarq/img19.png \
+   levmarq/img20.png levmarq/img21.png levmarq/img22.png levmarq/img23.png \
+   levmarq/img24.png levmarq/img25.png levmarq/img26.png levmarq/img27.png \
+   levmarq/img28.png levmarq/img29.png levmarq/img30.png levmarq/img31.png \
+   levmarq/img32.png levmarq/img33.png levmarq/img34.png levmarq/img35.png \
+   levmarq/img36.png levmarq/img37.png levmarq/img38.png levmarq/img39.png \
+   levmarq/img40.png levmarq/img41.png levmarq/img42.png levmarq/img43.png \
+   levmarq/img44.png levmarq/img45.png levmarq/img46.png levmarq/img47.png \
+   levmarq/img48.png levmarq/img49.png levmarq/img50.png levmarq/img51.png \
+   levmarq/img52.png levmarq/img53.png levmarq/img54.png levmarq/img55.png \
+   levmarq/img56.png levmarq/img57.png levmarq/img58.png levmarq/img59.png \
+   levmarq/img60.png levmarq/img61.png levmarq/img62.png levmarq/img63.png \
+   levmarq/img64.png levmarq/img65.png levmarq/img66.png levmarq/img67.png \
+   levmarq/img68.png levmarq/img69.png levmarq/img70.png levmarq/img71.png \
+   levmarq/img72.png levmarq/img73.png levmarq/img74.png levmarq/img75.png \
+   levmarq/img76.png levmarq/img77.png levmarq/img78.png levmarq/img79.png \
+   levmarq/img80.png levmarq/img81.png levmarq/img82.png levmarq/img83.png \
+   levmarq/img84.png \
+   levmarq/node1.html levmarq/node2.html levmarq/node3.html levmarq/node4.html\
+   levmarq/index.html levmarq/levmarq.html levmarq/levmarq.css \
+   levmarq/footnode.html levmarq.pdf \
+   contents.png next_g.png next.png prev_g.png prev.png up_g.png up.png
+
 
