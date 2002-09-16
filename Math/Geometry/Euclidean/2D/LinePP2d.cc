1cde61f570b9d1d2ee1fdb28215d44299e1d5711
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-09-16 12:32:08 +0000

    Make it compile for now...
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@1657 7226f50c-d813-0410-99bf-979b288ab488

--- Math/Geometry/Euclidean/2D/LinePP2d.cc
+++ Math/Geometry/Euclidean/2D/LinePP2d.cc
@@ -11,10 +11,11 @@
 
 namespace RavlN {
 
-  enum ContainCodeT { TOP=0x1,BOTTOM=0x2,RIGHT=0x4,LEFT=0x8 };
+#if 0
+  enum ContainCodeT { CC_NONE=0x0,TOP=0x1,BOTTOM=0x2,RIGHT=0x4,LEFT=0x8 };
   
   static inline ContainCodeT ContainsCode(const Point2dC &pnt,const RealRange2dC &rng) {
-    ContainCodeT ret = 0;
+    ContainCodeT ret = NONE;
     if(pnt[0] > rng.BRow())
       ret |= BOTTOM;
     if(pnt[0] < rng.TRow())
@@ -25,14 +26,17 @@ namespace RavlN {
       ret |= LEFT;
     return ret;
   }
+#endif
   
   //: Clip line by given rectangle.
   // Returns false if no part of the line is in the rectangle.
   
   bool LinePP2dC::ClipBy(const RealRange2dC &rng) {
+#if 0
     bool accept = false;
     bool done = false;
     ContainCodeT c0 = ContainsCode(P1(),rng);
+#endif
     return true;
   }
   
