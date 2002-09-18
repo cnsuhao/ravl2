42ed89c0c9af615cf556bab27dd6a51de7063040
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-09-18 14:57:05 +0000

    Tidy.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@1673 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Base/Index3d.hh
+++ Core/Base/Index3d.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef RAVLINDEX3D_HEADER
-#define RAVLINDEX3D_HEADER
+#ifndef RAVL_INDEX3D_HEADER
+#define RAVL_INDEX3D_HEADER
 ///////////////////////////////////////////////////////////////////
 //! file="Ravl/Core/Base/Index3d.hh"
 //! lib=RavlCore
@@ -30,60 +30,67 @@ namespace RavlN {
   {
   public:
     
+    //:----------------------------------------------
     // Constructor, assigment, copies, and destructor
-    // ----------------------------------------------
     
     explicit Index3dC(SizeT dim)
-      { RavlAssert(dim == 3); }
+    { RavlAssert(dim == 3); }
     //: Creates an index
     // This is for compatibilty with N-D indexes, which 
     // need to be told there dimentionality.
     
-    inline Index3dC(IndexC i = 0, IndexC j = 0, IndexC k = 0);
+    inline Index3dC(IndexC i = 0, IndexC j = 0, IndexC k = 0)
+    { Set(i, j, k); }
     // Creates index <i,j,k>.
-  
-    inline Index3dC(RealT ir, RealT jr, RealT kr);
+    
+    inline Index3dC(RealT ir, RealT jr, RealT kr) {
+      Set(IntT(ir),
+	  IntT(jr),
+	  IntT(kr));
+    }
     // Creates index <i,j,k> where i,j,k are cut value of ir, jr, kr 
     // respectivily.
     
-    inline Index3dC(const Index3dC & index);
-    // Copy constructor.
-    
-    inline void Set(IndexC i, IndexC j, IndexC k);
+    inline void Set(IndexC i, IndexC j, IndexC k) {
+      I() = i;
+      J() = j;
+      K() = k;
+    }
     // Sets this index to be <i,j,k>.
     
-    inline void Set(const Index3dC & index);
+    inline void Set(const Index3dC & index)
+    { *this = index; }
     // Sets this index to be equal to 'index'.
     
+    //:--------------------------
     // Access to the member items
-    // --------------------------
     
     inline IndexC I() const
-      { return (*this)[0]; }
+    { return (*this)[0]; }
     // Returns the first index.
     
     inline IndexC J() const
-      { return (*this)[1]; }
+    { return (*this)[1]; }
     // Returns the second index.
     
     inline IndexC K() const
-      { return (*this)[2]; }
+    { return (*this)[2]; }
     // Returns the third index.
     
     inline IndexC & I()
-      { return (*this)[0]; }
+    { return (*this)[0]; }
     // Access to the first index.
     
     inline IndexC & J()
-      { return (*this)[1]; }
+    { return (*this)[1]; }
     // Access to the second index.
     
     inline IndexC & K()
-      { return (*this)[2]; }
+    { return (*this)[2]; }
     // Access to the third index.    
     
+    //:-------------------------------------
     // Access to the neighbouring 3D indexes
-    // -------------------------------------
     
     inline Index3dC & Right();
     // Shifts the index to the right.
@@ -109,96 +116,48 @@ namespace RavlN {
     inline Index3dC DownN() const;
     // Returns the index of the down neighbour.
     
-    // Special member functions
-    // ------------------------
-        
   };
   
-  inline
-  void
-  Index3dC::Set(IndexC i, IndexC j, IndexC k) {
-    I() = i;
-    J() = j;
-    K() = k;
-  }
   
-  inline
-  Index3dC::Index3dC(IndexC i, IndexC j, IndexC k)
- { Set(i, j, k); }
-   
-  inline 
-  Index3dC::Index3dC(RealT ir,
-		     RealT jr,
-		     RealT kr) {
-    Set(IntT(ir),
-	IntT(jr),
-	IntT(kr));
-  }
-  
-  inline
-  Index3dC::Index3dC(const Index3dC & index)
-  { Set(index.I(), index.J(), index.K()); }
-  
-  inline
-  void
-  Index3dC::Set(const Index3dC & index)
-  { *this = index; }
- 
-  inline
-  Index3dC &
-  Index3dC::Right() {
+  inline Index3dC &Index3dC::Right() {
     J()++;
     return *this;
   }
   
-  inline
-  Index3dC & 
-  Index3dC::Left() {
+  inline Index3dC &Index3dC::Left() {
     J()--;
     return *this;
   }
   
-  inline
-  Index3dC &
-  Index3dC::Up() {
+  inline Index3dC &Index3dC::Up() {
     I()--;
     return *this;
   }
   
-  inline
-  Index3dC &
-  Index3dC::Down() {
+  inline Index3dC &Index3dC::Down() {
     I()++;
     return *this;
   }
   
-  inline
-  Index3dC
-  Index3dC::RightN() const {
+  inline Index3dC Index3dC::RightN() const {
     Index3dC id(*this);
     id.Right();
     return(id);
   }
   
-  inline
-  Index3dC
-  Index3dC::LeftN() const {
+  inline Index3dC Index3dC::LeftN() const {
     Index3dC id(*this);
     id.Left();
     return id;
   }
 
-  inline
-  Index3dC
-  Index3dC::UpN() const {
+  inline Index3dC Index3dC::UpN() const {
     Index3dC id(*this);
     id.Up();
     return id;
   }
   
-  inline
-  Index3dC
-  Index3dC::DownN() const {
+  inline Index3dC Index3dC::DownN() const {
     Index3dC id(*this);
     id.Down();
     return id;
