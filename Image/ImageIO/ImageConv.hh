bd2cecbcc97ce9de8051f02043f0c743bf90bcd7
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-10-30 09:33:31 +0000

    Fix include guard.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@1944 7226f50c-d813-0410-99bf-979b288ab488

--- Image/ImageIO/ImageConv.hh
+++ Image/ImageIO/ImageConv.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef RAVL_IMAGECNV_HEADER
-#define RAVL_IMAGECNV_HEADER 1
+#ifndef RAVLIMAGE_IMAGECONV_HEADER
+#define RAVLIMAEG_IMAGECONV_HEADER 1
 /////////////////////////////////////////////////////
 //! rcsid="$Id$"
 //! file="Ravl/Image/ImageIO/ImageConv.hh"
