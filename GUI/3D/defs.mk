c964cf387732da294a989e9c41ae747b90fbd6c5
vap-james <vap-james@7226f50c-d813-0410-99bf-979b288ab488>
2002-11-21 16:00:53 +0000

    change PROGLIBS for exView3D.cc
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@2056 7226f50c-d813-0410-99bf-979b288ab488

--- GUI/3D/defs.mk
+++ GUI/3D/defs.mk
@@ -26,36 +26,23 @@ endif
 
 DESCRIPTION = Interface to open GL.
 
-MAINS = 
-#exView3D.cc 
-#v3d.cc
-#ex3DCanvas.cc
+MAINS = #v3d.cc
 
 HEADERS = gdkgl.h gtkglarea.h gdkgl_gtk2.h gtkglarea_gtk2.h \
  DObject3D.hh DLight3D.hh Util.hh Canvas3D.hh DViewPoint3D.hh \
  DCube3D.hh DTriMesh3D.hh DPointSet3D.hh DPinholeCamera03D.hh \
  DTexTriMesh3D.hh View3D.hh
 
-#  DLines3D.hh
-#      
-
-# DPolops3D.hh Pixmap3D.hh
-
 SOURCES = gdkgl.c gtkglarea.c DObject3D.cc  DLight3D.cc \
  Util.cc Canvas3D.cc DViewPoint3D.cc DCube3D.cc DTriMesh3D.cc \
  DPointSet3D.cc DPinholeCamera03D.cc DTexTriMesh3D.cc View3D.cc
 
-#  DLines3D.cc
-#      
-
-# DPolops3D.cc Pixmap3D.cc
-
 PLIB = RavlGUI3D
 
-EXAMPLES = exCanvas3D.cc
+EXAMPLES = exCanvas3D.cc exView3D.cc
 
 USESLIBS = RavlCore RavlGeom RavlGUI OpenGL Ravl3D x11
 
 EHT = Ravl.GUI.3D.html
 
-PROGLIBS = Ravl3DIO RavlOS
+PROGLIBS = Ravl3DIO RavlOS RavlImageIO
