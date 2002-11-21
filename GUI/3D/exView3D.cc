bbfc20a77da9391073fe326b2ecdbdb411808e89
vap-james <vap-james@7226f50c-d813-0410-99bf-979b288ab488>
2002-11-21 16:01:34 +0000

    load TexTriMeshC from a specified input file
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@2057 7226f50c-d813-0410-99bf-979b288ab488

--- GUI/3D/exView3D.cc
+++ GUI/3D/exView3D.cc
@@ -1,4 +1,9 @@
-
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 //////////////////////////////////////////////////////
 //! rcsid="$Id$"
 
@@ -7,60 +12,42 @@
 #include "Ravl/GUI/View3D.hh"
 #include "Ravl/Option.hh"
 #include "Ravl/Vector3d.hh"
+#include "Ravl/3D/TexTriMesh.hh"
+#include "Ravl/GUI/DTexTriMesh3D.hh"
+#include "Ravl/DP/FileFormatIO.hh"
 
 #include <GL/gl.h>
 
 using namespace RavlN;
 using namespace RavlGUIN;
-
-void DrawFunc()
-{
-  Vector3dC diag(1,1,1);
-  Vector3dC d1 = diag/2;
-  Vector3dC d2 = -diag/2;
-
-  glBegin(GL_QUAD_STRIP);
-  {
-    glNormal3d(1,0,0);
-	       
-    glVertex3d(d1.X(),d1.Y(),d1.Y());
-    glVertex3d(d1.X(),d2.Y(),d1.Y());
-    
-    glVertex3d(d1.X(),d1.Y(),d2.Y());
-    glVertex3d(d1.X(),d2.Y(),d2.Y());
-  }
-  glEnd();
-
-  glBegin(GL_QUAD_STRIP);
-  {
-    glNormal3d(-1,0,0);
-    
-    glVertex3d(d2.X(),d1.Y(),d1.Y());
-    glVertex3d(d2.X(),d2.Y(),d1.Y());
-    
-    glVertex3d(d2.X(),d1.Y(),d2.Y());
-    glVertex3d(d2.X(),d2.Y(),d2.Y());
-  }
-  glEnd();  
-}
+using namespace Ravl3DN;
 
 int main(int nargs,char *args[]) 
 {
   OptionC opts(nargs,args); // Make sure help option is detected.
+  StringC file = opts.String("i","","input filename");
+  opts.Compulsory("i");
   opts.Check();
   
+  TexTriMeshC mesh;
+  if (!Load(file,mesh)) {
+    cerr << "Could not load input file " << file << endl;
+    return 1;
+  }
+
   Manager.Init(nargs,args);
   
   WindowC win(100,100,"Hello");  
   View3DC view(400,400);  
   win.Add(view);
-  win.Show();
+  win.Show();  
 
   cerr << "Starting gui. \n";
   Manager.Execute();
 
-  view.Put(DOpenGLC(Trigger(DrawFunc)));
+  view.Put(DTexTriMesh3DC(mesh));
   
   Manager.Wait();
 
+  return 0;
 }
