5ec73a528265207e5ad9638ea74edf62b868584b
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2002-11-22 15:42:45 +0000

    Add copyright notice.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@2065 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Filters/doThinPlateWarp.cc
+++ Image/Processing/Filters/doThinPlateWarp.cc
@@ -1,3 +1,12 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2002, OmniPerception Ltd.
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+//! rcsid="$Id$"
+//! lib=RavlImageProc
+//! author="Charles Galambos"
 
 #include "Ravl/Image/Image.hh"
 #include "Ravl/Image/WarpThinPlateSpline.hh"
