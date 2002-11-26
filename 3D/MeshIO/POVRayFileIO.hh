0f07aa55526b73931fea9fe69ad7a67d7e6faf91
vap-james <vap-james@7226f50c-d813-0410-99bf-979b288ab488>
2002-11-26 17:12:45 +0000

    POVRay File IO framework
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@2087 7226f50c-d813-0410-99bf-979b288ab488

---
+++ 3D/MeshIO/POVRayFileIO.hh
@@ -0,0 +1,105 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2002, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+#ifndef RAVL_POVRAYFILEIO_HEADER
+#define RAVL_POVRAYFILEIO_HEADER 1
+///////////////////////////////////////////////////////////////////
+//! rcsid="$Id$"
+//! lib=Ravl3DIO
+//! author="Jonathan Starck"
+//! docentry="Ravl.3D.IO"
+//! userlevel=Normal
+
+#include "Ravl/3D/TexTriMesh.hh"
+#include "Ravl/3D/TriMesh.hh"
+#include "Ravl/DP/Port.hh"
+#include "Ravl/Stream.hh"
+
+namespace Ravl3DN {
+  
+  //! userlevel=Develop
+  //: POVRay file IO - for POVRay meshes
+  
+  class DPOPOVRayFileBodyC
+    : public DPOPortBodyC<TriMeshC>
+  {
+  public:
+    DPOPOVRayFileBodyC(const StringC &fn);
+    //: Open file.
+    
+    DPOPOVRayFileBodyC(OStreamC &is);
+    //: Open stream.
+    
+    virtual bool Put(const TriMeshC &dat);
+    //: Put data.
+    
+    virtual bool IsPutReady() const;
+    //: Is port ready for data ?
+    
+  protected:
+    OStreamC outf;
+    bool done;
+  };
+  
+  //! userlevel=Advanced
+  //: POVRay file IO - for POVRay meshes
+
+  class DPOPOVRayFileC
+    : public DPOPortC<TriMeshC>
+  {
+  public:
+    DPOPOVRayFileC(const StringC &fn)
+      : DPEntityC(*new DPOPOVRayFileBodyC(fn))
+    {}
+    //: Open file.
+    
+    DPOPOVRayFileC(OStreamC &is)
+      : DPEntityC(*new DPOPOVRayFileBodyC(is))
+    {}
+    //: Open a stream.
+    
+  };
+  
+  //! userlevel=Develop
+  //: POVRay file IO - for POVRay meshes
+  
+  class DPOTexPOVRayFileBodyC
+    : public DPOPortBodyC<TexTriMeshC>
+  {
+  public:
+    DPOTexPOVRayFileBodyC(const StringC &fn);
+    //: Open file.
+    
+    virtual bool Put(const TexTriMeshC &dat);
+    //: Put data.
+    
+    virtual bool IsPutReady() const;
+    //: Is port ready for data ?
+    
+  protected:
+    OStreamC outf;
+    bool done;
+    StringC path;
+  };
+  
+  //! userlevel=Advanced
+  //: POVRay file IO - for POVRay meshes
+
+  class DPOTexPOVRayFileC
+    : public DPOPortC<TexTriMeshC>
+  {
+  public:
+    DPOTexPOVRayFileC(const StringC &fn)
+      : DPEntityC(*new DPOTexPOVRayFileBodyC(fn))
+    {}
+    //: Open file.
+    
+  };
+
+}
+
+
+#endif
