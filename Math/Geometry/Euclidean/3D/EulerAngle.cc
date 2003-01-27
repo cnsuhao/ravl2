f34b47e2803c89307725c20243d65d1ce73be2b7
plugger <plugger@7226f50c-d813-0410-99bf-979b288ab488>
2003-01-27 18:17:32 +0000

    now uses ravl macros instead of external macros
    VS: ----------------------------------------------------------------------
    
    
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@2318 7226f50c-d813-0410-99bf-979b288ab488

--- Math/Geometry/Euclidean/3D/EulerAngle.cc
+++ Math/Geometry/Euclidean/3D/EulerAngle.cc
@@ -24,7 +24,7 @@ namespace RavlN
     RealT cc = cos(a[2]);
 
   // epsilon
-#define SMALL_ANGLE (FLT_EPSILON*16)
+#define SMALL_ANGLE (RavlConstN::minReal*16)
 
   //---------------------------------------------
   // C-style euler angle -> matrix conversions
