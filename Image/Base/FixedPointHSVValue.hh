147013fb3f0d8f28375e27a5ec29c886f3de30b6
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2003-03-06 17:24:58 +0000

    Add DrawMask.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@2509 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Base/FixedPointHSVValue.hh
+++ Image/Base/FixedPointHSVValue.hh
@@ -1,5 +1,13 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2003, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 #ifndef RAVL_FIXED_POINT_HSV_VALUE_HEADER
 #define RAVL_FIXED_POINT_HSV_VALUE_HEADER 1
+//! rcsid="$Id$"
+//! lib=RavlImage
 
 #include "Ravl/Stream.hh"
 #include "Ravl/Image/ByteRGBValue.hh"
