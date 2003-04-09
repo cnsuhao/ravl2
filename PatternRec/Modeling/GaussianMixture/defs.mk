ffac93eb5774d9f044b29312d558a5d5e6361e86
kier <kier@7226f50c-d813-0410-99bf-979b288ab488>
2003-04-09 08:49:51 +0000

    oops
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@2696 7226f50c-d813-0410-99bf-979b288ab488

---
+++ PatternRec/Modeling/GaussianMixture/defs.mk
@@ -0,0 +1,28 @@
+# This file is part of RAVL, Recognition And Vision Library 
+# Copyright (C) 2002, University of Surrey
+# This code may be redistributed under the terms of the GNU Lesser
+# General Public License (LGPL). See the lgpl.licence file for details or
+# see http://www.gnu.org/copyleft/lesser.html
+# file-header-ends-here
+#! rcsid="$Id$"
+#! file="Ravl/PatternRec/Modeling/GaussianMixture/defs.mk"
+
+DESCRIPTION = Gaussian Mixture Modelling
+
+PACKAGE = Ravl/PatternRec
+
+MAINS=testGaussianMixture.cc 
+
+HEADERS=GaussianMixture.hh DesignGaussianMixture.hh
+
+SOURCES=GaussianMixture.cc DesignGaussianMixture.cc
+
+PLIB=RavlPatternRec
+
+LIBDEPS=
+
+USESLIBS=RavlCore RavlMath RavlOS RavlPatternRec
+
+AUXFILES=class5.strm
+
+AUXDIR=share/RAVL/data
