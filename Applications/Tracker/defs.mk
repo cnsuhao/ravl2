0c63cbf60b6e43b6ac65b63099648ff8dfb560b2
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2003-04-24 17:05:42 +0000

    Added more contrib libraries
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@2790 7226f50c-d813-0410-99bf-979b288ab488

--- Applications/Tracker/defs.mk
+++ Applications/Tracker/defs.mk
@@ -12,6 +12,6 @@ DONOT_SUPPORT=arm
 EXAMPLES=mosaic.cc
 MAINS=tracker.cc mosaic.cc xmosaic.cc
 
-PROGLIBS=RavlImageProc RavlVideoIO RavlGUI2D RavlImgIOV4L.opt RavlDPDisplay.opt CSPDriver.opt
+PROGLIBS=RavlImageProc RavlVideoIO RavlGUI2D RavlImgIOV4L.opt RavlDPDisplay.opt CSPDriver.opt RavlDV.opt RavlExtImgIO.opt RavlImageIO RavlMPEG2.opt CSPDriver.opt RavlURLIO.opt
 
 EHT = Ravl.Applications.Mosaic.mosaic.html Ravl.Applications.Mosaic.xmosaic.html Ravl.Applications.tracker.html
