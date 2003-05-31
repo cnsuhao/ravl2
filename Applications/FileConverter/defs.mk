ba0f514b1d1b3ac80aae3847d1c5d73616d4ba1b
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2003-05-31 10:07:20 +0000

    Include RavlAudioIO.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@3000 7226f50c-d813-0410-99bf-979b288ab488

--- Applications/FileConverter/defs.mk
+++ Applications/FileConverter/defs.mk
@@ -16,6 +16,6 @@ USESLIBS= RavlCore RavlOSIO RavlImage RavlOS RavlImageIO RavlVideoIO  \
 
 PROGLIBS= RavlDPDisplay.opt RavlExtImgIO.opt RavlDV.opt RavlImgIOV4L.opt  \
  RavlPatternRecIO RavlMPEG2.opt CSPDriver.opt RavlURLIO.opt RavlDevAudio.opt \
- RavlAudioFile.opt RavlDMeda.opt RavlImgIOMeteor1.opt RavlDPGraph.opt
+ RavlAudioFile.opt RavlDMeda.opt RavlImgIOMeteor1.opt RavlDPGraph.opt RavlAudioIO.opt
 
 EHT = execonv.eht
