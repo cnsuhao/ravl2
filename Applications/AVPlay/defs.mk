5af42a3597d7950d92cb21f4b2eabe3a4d7ddd6c
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2003-07-17 21:55:44 +0000

    Initial version.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@3185 7226f50c-d813-0410-99bf-979b288ab488

---
+++ Applications/AVPlay/defs.mk
@@ -0,0 +1,21 @@
+# This file is part of RAVL, Recognition And Vision Library 
+# Copyright (C) 2001, University of Surrey
+# This code may be redistributed under the terms of the GNU Lesser
+# General Public License (LGPL). See the lgpl.licence file for details or
+# see http://www.gnu.org/copyleft/lesser.html
+# file-header-ends-here
+###############################################
+#! rcsid="$Id$"
+#! file="Ravl/Applications/VPlay/defs.mk"
+
+DESCRIPTION = AVPlay - Audio / Video Player.
+
+DONOT_SUPPORT=arm
+
+PACKAGE = Ravl/VPlay
+
+MAINS= AVPlay.cc 
+
+PROGLIBS = RavlAV RavlVPlay RavlDV.opt RavlImgIOV4L.opt RavlExtImgIO RavlDPDisplay \
+ RavlImageIO RavlVideoIO CSPDriver.opt RavlURLIO.opt RavlAudioFile.opt RavlDevAudio RavlAudioIO
+
---
+++ Image/AV/AVFrame.hh
@@ -0,0 +1,56 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2003, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+#ifndef RAVLIMAGE_AVFRAME_HEADER
+#define RAVLIMAGE_AVFRAME_HEADER 1
+//! rcsid="$Id$"
+//! lib=RavlAV
+//! author="Charles Galambos"
+//! userlevel=Normal
+
+#include "Ravl/Image/Image.hh"
+#include "Ravl/Audio/AudioFrame.hh"
+#include "Ravl/Image/ByteRGBValue.hh"
+
+namespace RavlImageN {
+  using namespace RavlAudioN;
+  
+  //! userlevel=Normal
+  //: Audio-Video frame.
+  
+  class AVFrameC
+  {
+  public:
+    AVFrameC()
+    {}
+    //: Default constructor.
+    
+    AVFrameC(const AudioFrameC &audioData,ImageC<ByteRGBValueC> &img);
+    //: Constructor.
+
+    AudioFrameC &Audio()
+    { return audio; }
+    //: Access audio data.
+    
+    const AudioFrameC &Audio() const
+    { return audio; }
+    //: Access audio data.
+    
+    ImageC<ByteRGBValueC> &Image()
+    { return image; }
+    //: Image. 
+
+    const ImageC<ByteRGBValueC> &Image() const
+    { return image; }
+    //: Image. 
+    
+  protected:
+    AudioFrameC audio;
+    ImageC<ByteRGBValueC> image;
+  };
+}
+
+#endif
