1f1715184652399e9da457d8df7671b34d2339b7
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2003-08-05 15:18:16 +0000

    Empty shell.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@3280 7226f50c-d813-0410-99bf-979b288ab488

---
+++ PatternRec/Optimise/OptimiseDirectionSet.cc
@@ -0,0 +1,70 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2003, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+//! rcsid="$Id$"
+//! lib=Optimisation
+//! file="Ravl/PatternRec/Optimise/OptimiseSobol.cc"
+
+#include "Ravl/PatternRec/OptimiseDirectionSet.hh"
+#include "Ravl/StrStream.hh"
+
+namespace RavlN {
+
+  OptimiseDirectionSetBodyC::OptimiseDirectionSetBodyC (const SArray1dC<VectorC> &initState)
+    : OptimiseBodyC("OptimiseDirectionSetBodyC"),
+      state(initState)
+  {}
+  
+  OptimiseDirectionSetBodyC::OptimiseDirectionSetBodyC (istream &in)
+    : OptimiseBodyC("OptimiseDirectionSetBodyC",in)
+  {}
+  
+  // Random optimizer with uniform density. Randomly samples the parameter
+  // space to find the minimum cost position.
+  
+  VectorC OptimiseDirectionSetBodyC::MinimalX (const CostC &domain)
+  {
+    VectorC start = domain.StartX();
+    UIntT n = start.Size();
+    SArray1dC<VectorC> u(n);
+    
+    // Initialise u.
+    for(UIntT i = 0;i < u;i++) {
+      u.Fill(0);
+      u[i] = 1;
+    }
+    
+    RavlAssert(0);
+    //return domain.ConvertX2P (currentX);     // Return final estimate
+  }
+  
+  
+  //: Find minimum along direction 'dir' from start, 
+  
+  VectorC OptimiseDirectionSetBodyC::FindMin(const VectorC &start,const VectorC &dir) {
+    VectorC P = start;
+    RealT p1 = domain.Cost (P + dir);
+    RealT p2 = domain.Cost (P);
+    RealT p3 = domain.Cost (P - dir);
+    
+    
+    return at;
+  }
+
+  const StringC OptimiseDirectionSetBodyC::GetInfo () const
+  {
+    StrOStreamC stream;
+    stream << OptimiseBodyC::GetInfo () << "\n";
+    stream << "DirectionSet parameter space search optimisation. ";
+    return stream.String();
+  }
+  
+  bool OptimiseDirectionSetBodyC::Save (ostream &out) const {
+    OptimiseBodyC::Save (out);
+    return true;
+  }
+  
+}
---
+++ PatternRec/Optimise/OptimiseDirectionSet.hh
@@ -0,0 +1,75 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2003, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+#ifndef RAVL_OPTIMISEDIRECTIONSET_HEADER
+#define RAVL_OPTIMISEDIRECTIONSET_HEADER 1
+////////////////////////////////////////////////////////////////////////////
+//! author="Robert Crida"
+//! lib=Optimisation
+//! date="22/4/1998"
+//! userlevel=Normal
+//! example=testNumOptimise.cc
+//! docentry="Ravl.Pattern Recognition.Optimisation.Implementation"
+//! rcsid="$Id$"
+//! file="Ravl/PatternRec/Optimise/OptimiseSobol.hh"
+
+#include "Ravl/PatternRec/Optimise.hh"
+#include "Ravl/SArray1d.hh"
+
+namespace RavlN {
+  
+  //! userlevel=Develop
+  //: DirectionSet distribution search optimiser implementation class.
+  //
+  // This is the implementation class of the sobol distribution search
+  // optimiser for the PatternRec toolbox. The OptimiseDirectionSetC handle
+  // class should be used.
+  
+  class OptimiseDirectionSetBodyC: public OptimiseBodyC
+  {
+  public:
+    OptimiseDirectionSetBodyC ();
+    //: Default constructor
+    //!param: numSamples - number of samples to check
+    
+    OptimiseDirectionSetBodyC (istream &in);
+    //: Constructs from stream
+    
+  protected:
+    VectorC MinimalX (const CostC &domain);
+    //: Determines Xmin=arg min_{X} |f(X)-Yd|
+    
+    VectorC FindMin(const VectorC &start,const VectorC &dir);
+    //: Find minimum along direction 'dir' from start, 
+    
+    virtual const StringC GetInfo () const;
+    //: Prints information about the optimizer
+    
+    virtual bool Save (ostream &out) const;
+    //: Writes object to stream, can be loaded using constructor
+    
+    SArray1dC<VectorC> u;
+  };
+  
+  //! userlevel=Normal
+  //: DirectionSet distribution search optimisation.
+  //
+  // Class for performing a search using a sobol distribution.
+  
+  class OptimiseDirectionSetC: public OptimiseC
+  {
+  public:
+    OptimiseDirectionSetC ()
+      : OptimiseC(*(new OptimiseDirectionSetBodyC (initState)))
+    {}
+    //: Constructor
+    //!param: numSamples - number of samples to check
+  };
+  
+}
+
+#endif
+
