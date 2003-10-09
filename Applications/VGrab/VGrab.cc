2e7cb3e590dda714f607288fd5c9f70f7db71007
plugger <plugger@7226f50c-d813-0410-99bf-979b288ab488>
2003-10-09 15:48:16 +0000

    Now VGrab inserts blank images when frames are dropped.
    Also some minor changes to make the program more scriptable
    
    
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@3526 7226f50c-d813-0410-99bf-979b288ab488

--- Applications/VGrab/VGrab.cc
+++ Applications/VGrab/VGrab.cc
@@ -42,9 +42,9 @@ int VGrab(int argc, char ** argv)
   StringC usage = "This program captures video from grabber cards. The grabber device can be specified using RAVL virtual files and defaults to the ClipStationPro driver. \n Output formats are determined by the extension specified, 4cif is the defalut." ; 
   opts.Comment(usage) ; 
 
-  RealT delay       = opts.Real    ("delay", 0.0,               "The delay in seconds " ) ; 
+  IntT  delay       = opts.Int     ("delay", 0  ,               "The delay in seconds " ) ; 
   bool showTC       = opts.Boolean ("g",     false,             "Get current timecode " ) ; 
-  bool verbose      = opts.Boolean ("v",     false,             "Be verbose ? " ) ; 
+  bool verbose      = opts.Boolean ("v",     true,             "Be verbose ? " ) ; 
   StringC start     = opts.String  ("start", "now",             "The starting timecode [HH:MM:SS:FF , now] " ) ; 
   StringC end       = opts.String  ("end",   "",                "The ending timecode [HH:MM:SS:FF] ") ; 
   FilenameC outFile = opts.String  ("out",   "tmp1.4cif",       "The output filename for capture device 1 (.cif, .4cif,  .ppm, .avi, .jpg ) ") ; 
@@ -56,7 +56,6 @@ int VGrab(int argc, char ** argv)
   opts.Check() ;  
 
 
-  Sleep (delay) ; 
 
 
   //: create a name for the timecode file 
@@ -65,7 +64,7 @@ int VGrab(int argc, char ** argv)
   if ( outFile.PathComponent() == "" ) tcFile = outFile.BaseNameComponent()+".tc" ;
   else 
     tcFile = outFile.PathComponent()  + "/" + outFile.BaseNameComponent()+".tc" ; 
-  //cerr << "tcFile -= " << tcFile ; 
+  //cout << "tcFile -= " << tcFile ; 
   OStreamC tcStream(tcFile) ; 
 
 
@@ -73,11 +72,11 @@ int VGrab(int argc, char ** argv)
 
   // try to setup the capture device 
   // --------------------------------
-  cerr << "\n   -  Trying to setup capture device " << device << " ....  " ; 
+  if (verbose) cout << "\n   -  Trying to setup capture device " << device << " ....  " ; 
   DPIPortC<ImageT> inStream ; 
   if ( ! OpenISequence ( inStream, device, "", verbose) ) 
     { RavlIssueError ("\n   -  Failed to open input device, exiting .... " ) ; } 
-  cerr << "\t\tdone  ! " ; 
+  if (verbose) cout << "\t\tdone  ! " ; 
 
 
 
@@ -88,10 +87,10 @@ int VGrab(int argc, char ** argv)
   DListC<ImageT> imgList ;
   DListC<TimeCodeC> tcList ; 
 
-
+  
   // check timecode support for device 
   // -------------------------
-  cerr << "\n   -  Checking Timecode Support .... " ;
+  if (verbose ) cout << "\n   -  Checking Timecode Support .... " ;
   bool hasTimecode = false ; 
   bool useTimecode = false ;
   
@@ -106,16 +105,20 @@ int VGrab(int argc, char ** argv)
   if (timeNow!="00:00:00:00" && timeNow!="")  //: BUG - What if the timecode really is 00:00:00:00 ! 
     useTimecode = true ; 
 
+
+
   // display some status 
-  if ( hasTimecode ) 
-    cerr << "\n     -  Device supports timecodes " ; 
-  else 
-    cerr << "\n     -  Device does not support timecodes " ; 
-  
-  if ( useTimecode ) 
-    cerr << "\n     -  Timecode read successfully from video stream " ; 
-  else 
-    cerr << "\n     -  Unable to read timecode from video stream " ; 
+  if (verbose) {
+    if ( hasTimecode ) 
+      cout << "\n     -  Device supports timecodes " ; 
+    else 
+      cout << "\n     -  Device does not support timecodes " ; 
+    
+    if ( useTimecode ) 
+      cout << "\n     -  Timecode read successfully from video stream " ; 
+    else 
+      cout << "\n     -  Unable to read timecode from video stream " ; 
+  }
 
   
   // do some checks 
@@ -128,13 +131,15 @@ int VGrab(int argc, char ** argv)
   // ----------------------------
   if ( showTC ) {
     if ( hasTimecode ) 
-      cerr << "\n\n   -  The Current timecode is " << timeNow.ToText() << "\n\n" ; 
-    else 
-      cerr << "\n\n   - Option -g is invalid since device " << device 
-	   << " does not have timecode support\n\n " ;
+      if (verbose) 
+	cout << "\n\n   -  The Current timecode is " << timeNow.ToText() << "\n\n" ; 
+      else 
+	cout << timeNow.ToText() ; 
+      else 
+	RavlIssueError ("\n\n   - Option -g is invalid since device "+device+" does not have timecode support\n\n ") ;
     return 0 ; 
   }
-      
+  
 
 
   // try to open the output stream 
@@ -147,33 +152,44 @@ int VGrab(int argc, char ** argv)
 
   // Wait for timecode 
   // -----------------
-  cerr << "\n\n   -  Waiting for timecode ...."  ; 
+  if (verbose) cout << "\n\n   -  Waiting for timecode ...."  ; 
   //: Wait for timecode 
   IntT statusStep = 0 ; 
   if ( start != "now" )
-  while (timeNow < TimeCodeC(start)-1) {
-    tmpImage = inStream.Get() ; 
-    timeNow =  inStream.GetAttr("timecode") ;  
-    ++ statusStep ; 
+    {
+      TimeCodeC startTC (start) ;
+      startTC -= 1 ; 
+      startTC += IntT (delay * startTC.FrameRate()) ; 
+
+      //cout << startTC.ToText() ;
+
+      if (timeNow > startTC ) 
+	{ cout <<  "timecode has been missed " ; return (1) ; } 
+
+      while (timeNow < startTC ) {
+	cout << "\n" << timeNow.ToText() ; 
+	tmpImage = inStream.Get() ; 
+	timeNow =  inStream.GetAttr("timecode") ;  
+	++ statusStep ; 
     
-    // display status every 100th frame 
-    TimeCodeC timeLeft (0) ;
-    if (statusStep >= 100 ) {
-      timeLeft = TimeCodeC(start) - timeNow ; 
-      cerr << "\n    -  Time now: " << timeNow.ToText()  << "\t Waiting for: " << (TimeCodeC(start)-1).ToText() << "\t\tTime remaining: " 
-	   << timeLeft.ToText() ;  
-      statusStep = 0 ; } 
-  }
+	// display status every 100th frame 
+	TimeCodeC timeLeft (0) ;
+	if (statusStep >= 100 ) {
+	  timeLeft = startTC - timeNow ; 
+	  if (verbose) 
+	    cout << "\n    -  Time now: " << timeNow.ToText()  << "\t Waiting for: " << startTC.ToText() << "\t\tTime remaining: " << timeLeft.ToText() ;  
+	  statusStep = 0 ; } 
+      }}
 
   
-  if ( ! hasTimecode ) cerr << "\n   - Device: " << device << " has no timecode support, unable to detect dropped frames " ;  
-
-
+  if ( (!hasTimecode) && verbose ) cout << "\n   - Device: " << device << " has no timecode support, unable to detect dropped frames " ;  
+  if ( (!hasTimecode) ) Sleep(delay) ;
+  
 
   //   Start capture 
   // -----------------
   // -----------------
-  cerr << "\n\n   - Starting capture " ; 
+  if (verbose) cout << "\n\n   - Starting capture " ; 
  
   
   // using timecodes 
@@ -183,22 +199,22 @@ int VGrab(int argc, char ** argv)
       TimeCodeC nextGrab = timeNow + 1 ; 
       TimeCodeC endTime = end ; 
       if ( !opts.IsOnCommandLine("end") ) endTime = nextGrab + howMany - 1; 
-      cerr << "\n   -  First Grab will be: " << nextGrab.ToText() << "  \t and endtime is: " << endTime.ToText() ; 
+      if (verbose) cout << "\n   -  First Grab will be: " << nextGrab.ToText() << "  \t and endtime is: " << endTime.ToText() ; 
 
       while ( true ) 
       {
 	//Sleep ( .5) ; 
-	//cerr << "\n Time now " << timeNow.ToText() << "end is " << endTime.ToText() << " cond = " << (timeNow>=nextGrab) ; 
+	//cout << "\n Time now " << timeNow.ToText() << "end is " << endTime.ToText() << " cond = " << (timeNow>=nextGrab) ; 
 	if (timeNow >= endTime) break ; 
 	tmpImage = inStream.Get() ; 
 	timeNow =  inStream.GetAttr("timecode") ;  
 	// decide if we want this frame 
 	if ( timeNow >= nextGrab ) 
 	  {
-	    if (verbose) cerr << "\n     -  Grabbed frame " << timeNow.ToText() << "\t expected: " << nextGrab.ToText() ; 
+	    if (verbose) cout << "\n     -  Grabbed frame " << timeNow.ToText() << "\t expected: " << nextGrab.ToText() ; 
 
 	    if ( timeNow != nextGrab ) {
-	      cerr << "\n  *** Dropped frame with timecode " << nextGrab.ToText() 
+	      cout << "\n  *** Dropped frame with timecode " << nextGrab.ToText() 
 		   << "\t Grabbing " << timeNow.ToText() << " instead !" ; 
 	      nextGrab = timeNow ; 
 	    }
@@ -239,10 +255,10 @@ int VGrab(int argc, char ** argv)
 
   // Save image sequence 
   // ---------------------
-  cerr << "\n   - Saving " << imgList.Size() << " frames .... " ; 
+  if (verbose) cout << "\n   - Saving " << imgList.Size() << " frames .... " ; 
   for ( DLIterC<ImageT> iter (imgList) ; iter.IsElm() ; iter.Next() )
     outStream.Put( iter.Data() ) ; 
-  cerr << "\t\t\t\tdone! " ; 
+  if (verbose) cout << "\t\t\t\tdone! " ; 
 
   // Save timecodes 
   // ----------------
