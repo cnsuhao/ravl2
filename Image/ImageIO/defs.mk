83fd323396d5d8f39ec17b1d731e4963773a45cc
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2003-11-19 20:33:34 +0000

    Fix accidental checkin.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@3704 7226f50c-d813-0410-99bf-979b288ab488

--- Image/ImageIO/defs.mk
+++ Image/ImageIO/defs.mk
@@ -11,16 +11,14 @@ PACKAGE = Ravl/Image
 
 DESCRIPTION =  File IO methods.
 
-HEADERS = ImgIOPNMB.hh ImgIOPNM.hh PNMFormat.hh ImgIO.hh \
- VideoCapture.hh
+HEADERS = ImgIOPNMB.hh ImgIOPNM.hh PNMFormat.hh ImgIO.hh 
 
 SOURCES = ImgIOPNM.cc PNMFormat.cc ImgIOInt.cc ImgIOByte.cc \
  ImgIOUInt16.cc ImgIOByteRGB.cc ImgIOByteYUV.cc ImgIOReal.cc \
  ImgIORealRGB.cc ImgIOByteRGBA.cc ImgIORealYUV.cc ImgIOUInt16RGB.cc \
  ImgIOByteYUVA.cc ImgCnvRGB.cc ImgCnvYUV.cc ImgIOFloat.cc \
  ImgTypeCnv.cc ImgTypeCnv2.cc ImgTypeCnv3.cc ImgTypeCnv4.cc \
- ImgIOByteYUV422.cc ImgIOByteVYU.cc ImgTypeCnv5.cc \
- VideoCapture.cc
+ ImgIOByteYUV422.cc ImgIOByteVYU.cc ImgTypeCnv5.cc 
 
 # ImgIOPixel.cc  PixelIO.cc 
 
