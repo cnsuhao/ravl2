ca66daeaf8c40426a00a5824ff72a0ff07952aec
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2003-11-25 10:40:46 +0000

    Added docentry
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@3729 7226f50c-d813-0410-99bf-979b288ab488

--- Core/System/StringList.hh
+++ Core/System/StringList.hh
@@ -11,7 +11,7 @@
 //! lib=RavlCore
 //! userlevel=Normal
 //! author="Bill Christmas"
-//! docentry="Ravl.Core.Lists"
+//! docentry="Ravl.Core.Lists;Ravl.Core.Strings"
 //! rcsid="$Id$"
 //! date="12/07/1996"
 
