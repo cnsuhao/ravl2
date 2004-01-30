e0948a82aff3fd382d9e572cff0a50f127947b76
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2004-01-30 10:53:17 +0000

    Minor fix.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@3911 7226f50c-d813-0410-99bf-979b288ab488

--- Core/System/CollectionIter.hh
+++ Core/System/CollectionIter.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef RAVLCOLLECTIONITER_HEADER
-#define RAVLCOLLECTIONITER_HEADER 1
+#ifndef RAVL_COLLECTIONITER_HEADER
+#define RAVL_COLLECTIONITER_HEADER 1
 //////////////////////////////////////////////////////////////
 //! rcsid="$Id$"
 //! docentry="Ravl.Core.Misc"
