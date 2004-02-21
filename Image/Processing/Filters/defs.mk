f699a8fd82b891706c38855dc582f7101eb0878f
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2004-02-21 12:04:33 +0000

    Add normalised correlation function.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@3980 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Filters/defs.mk
+++ Image/Processing/Filters/defs.mk
@@ -16,7 +16,7 @@ HEADERS= Convolve2d.hh ConvolveSeparable2d.hh ConvolveHorz2d.hh \
  Correlate2d.hh Matching.hh WarpScale.hh WarpAffine.hh WarpProjective.hh \
  PixelMixer.hh GaussConvolve.hh RemoveDistortion.hh WarpThinPlateSpline.hh \
  SpatialDifference.hh  SumRectangles.hh DeinterlaceStream.hh GabourFilter2d.hh \
- DCT2d.hh
+ DCT2d.hh NormalisedCorrelation.hh
 
 SOURCES= HomomorphicFilter.cc Correlate2d.cc Matching.cc WarpThinPlateSpline.cc \
  GabourFilter2d.cc DCT2d.cc DeinterlaceStream.cc
