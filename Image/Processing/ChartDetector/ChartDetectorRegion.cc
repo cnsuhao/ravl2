d17aafb96b7e29e663cd0c798b8d6269092e8bfc
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2004-02-21 12:03:32 +0000

    Initial version.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@3979 7226f50c-d813-0410-99bf-979b288ab488

---
+++ Image/Processing/ChartDetector/ChartDetectorRegion.cc
@@ -0,0 +1,51 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2003, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+//! rcsid="$Id$"
+//! lib=RavlChartDetector
+
+#include "Ravl/Image/ChartDetectorRegion.hh"
+#include "Ravl/IO.hh"
+#include "Ravl/Boundary.hh"
+
+#define DODEBUG 1
+
+#if DODEBUG
+#define ONDEBUG(x) x
+#else
+#define ONDEBUG(x)
+#endif
+
+namespace RavlImageN {
+  
+  //: Region model.
+  
+  ChartDetectorRegionBodyC::ChartDetectorRegionBodyC(const SegmentationC &aSegmentation,UIntT nLabel,UIntT nRegionType)
+    : segmentation(aSegmentation),
+      label(nLabel),
+      regionType(nRegionType)
+  { UpdateStats(); }
+  
+  //: Update stats on region.
+  
+  void ChartDetectorRegionBodyC::UpdateStats() {
+    // Compute the number of holes in the region.
+    BoundaryC boundry(static_cast<Array2dC<UIntT> >(segmentation.SegMap()),1);
+    DListC<BoundaryC> edges = boundry.OrderEdges();
+    if(edges.Size() == 0) {
+      Save("@X:Region",segmentation.SegMap());
+      cerr << "ChartDetectorRegionBodyC::UpdateStats(), WARNING: Zero boundries for region ?!? \n";
+      holes = 0;
+    } else
+      holes = edges.Size() - 1;
+    //cerr << "Holes=" << holes << "\n";
+    // Could do more with the moments...
+    SArray1dC<Moments2d2C> momentArray = segmentation.ComputeMoments(true);
+    centroid = momentArray[1].Centroid();
+  }
+  
+  
+}
---
+++ Image/Processing/ChartDetector/chartdet.cc
@@ -0,0 +1,55 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2003, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+//! rcsid="$Id$"
+//! lib=RavlChartDetector
+
+#include "Ravl/Image/ChartDetector.hh"
+#include "Ravl/IO.hh"
+#include "Ravl/Option.hh"
+
+using namespace RavlN;
+using namespace RavlImageN;
+
+int main(int nargs,char **argv) {
+  OptionC opt(nargs,argv);
+  RealT inlierThreshold = opt.Real("id",10,"Inlier distance threshold. ");
+  UIntT ransacIterations = (UIntT) opt.Int("ri",10000,"RANSAC iterations to use in search.");
+  RealT acceptThreshold = opt.Real("at",0.7,"Threshold at which to just accept hypothesis ");
+  StringC chartName = opt.String("c","chart.pgm","Image of chart to search for. ");
+  StringC sceneName = opt.String("","image.pgm","Image in which to search for chart. ");
+  
+  UIntT minRegionSize = 40;
+  UIntT minRegionMargin = 10;
+  opt.Check();
+  
+  ImageC<ByteT> chartImage;
+  if(!Load(chartName,chartImage)) {
+    cerr << "Failed to load image of chart. \n";
+    return 1;
+  }
+  
+  ImageC<ByteT> sceneImage;
+  if(!Load(sceneName,sceneImage)) {
+    cerr << "Failed to load image to search. \n";
+    return 1;
+  }
+  
+  ChartDetectorC chartDetector(chartImage,minRegionSize,minRegionMargin);
+  chartDetector.SetInlierThreshold(inlierThreshold);
+  chartDetector.SetRANSACIterations(ransacIterations);
+  chartDetector.SetAcceptThreshold(acceptThreshold);
+  
+  Projection2dC transform;  
+  if(!chartDetector.Apply(sceneImage,transform)) {
+    cerr << "Failed to find the chart in the image. ";
+    return 2;
+  }
+  
+  cerr << "Transform =" << transform << "\n";
+  
+  return 0;
+}
