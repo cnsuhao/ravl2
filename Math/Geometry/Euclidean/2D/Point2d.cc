20fc3792b9382b2be73d87ee0117d269bdf5da44
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2004-03-15 14:28:27 +0000

    Make Area2() an inline func.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@4085 7226f50c-d813-0410-99bf-979b288ab488

--- Math/Geometry/Euclidean/2D/Point2d.cc
+++ Math/Geometry/Euclidean/2D/Point2d.cc
@@ -11,15 +11,5 @@
 #include "Ravl/Point2d.hh"
 
 namespace RavlN {
-   RealT Point2dC::Area2(const Point2dC & second, const Point2dC & third) const {
-      // Area of tringle (*this, second, third) is equal to the area
-      // of the tringle which the first point represents the origin
-      // of the coordinate system. In fact the points 'aa' and 'bb'
-      // represents two vectors and the computed area is equal to
-      // the size of the cross product of these two vectors.
-      Point2dC aa(second - *this);   // O'Rourke 1.2
-      Point2dC bb(third  - *this);
-      return aa[0]*bb[1] - aa[1]*bb[0];
-   }
 
 }
--- Math/Geometry/Euclidean/2D/Point2d.hh
+++ Math/Geometry/Euclidean/2D/Point2d.hh
@@ -83,10 +83,19 @@ namespace RavlN {
     RealT Col() const
     { return data[1]; }
     //: Second component of vector.
-
-    RealT Area2(const Point2dC& second, const Point2dC& third) const;
+    
+    RealT Area2(const Point2dC& second, const Point2dC& third) const {
+      // Area of tringle (*this, second, third) is equal to the area
+      // of the tringle which the first point represents the origin
+      // of the coordinate system. In fact the points 'aa' and 'bb'
+      // represents two vectors and the computed area is equal to
+      // the size of the cross product of these two vectors.
+      Point2dC aa(second - *this);   // O'Rourke 1.2
+      Point2dC bb(third  - *this);
+      return aa[0]*bb[1] - aa[1]*bb[0];
+    }
     //: return twice the area contained by the three points
-
+    
   };
 }
 
