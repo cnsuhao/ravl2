92092d739be2a8360efaea686865c76662424525
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2004-03-18 17:16:56 +0000

    Reordered headers in order to reorder classes in doc
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@4109 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Filters/defs.mk
+++ Image/Processing/Filters/defs.mk
@@ -12,11 +12,11 @@ PACKAGE=Ravl/Image
 MAINS=doFilter.cc unDistort.cc doThinPlateWarp.cc 
 
 HEADERS= Convolve2d.hh ConvolveSeparable2d.hh ConvolveHorz2d.hh \
- ConvolveVert2d.hh HomomorphicFilter.hh HistogramEqualise.hh \
- Correlate2d.hh Matching.hh WarpScale.hh WarpAffine.hh WarpProjective.hh \
- PixelMixer.hh GaussConvolve2d.hh GaussConvolve.hh RemoveDistortion.hh WarpThinPlateSpline.hh \
- SpatialDifference.hh  SumRectangles.hh DeinterlaceStream.hh GaborFilter2d.hh GabourFilter2d.hh \
- DCT2d.hh NormalisedCorrelation.hh
+ ConvolveVert2d.hh GaussConvolve2d.hh GaussConvolve.hh Correlate2d.hh NormalisedCorrelation.hh GaborFilter2d.hh GabourFilter2d.hh HomomorphicFilter.hh HistogramEqualise.hh \
+ Matching.hh WarpScale.hh WarpAffine.hh WarpProjective.hh \
+ PixelMixer.hh RemoveDistortion.hh WarpThinPlateSpline.hh \
+ SpatialDifference.hh  SumRectangles.hh DeinterlaceStream.hh \
+ DCT2d.hh
 
 SOURCES= HomomorphicFilter.cc Correlate2d.cc Matching.cc WarpThinPlateSpline.cc \
  GaborFilter2d.cc DCT2d.cc DeinterlaceStream.cc
