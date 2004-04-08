7f2560f2c1d07a9254211f1077b7a36f080e6f27
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2004-04-08 19:19:55 +0000

    Atomic refrence counting for Visual C++.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@4211 7226f50c-d813-0410-99bf-979b288ab488

---
+++ Core/Machine/i386-win32-vc/Atomic.hh
@@ -0,0 +1,56 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2004, OmniPerception Ltd.
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+#ifndef RAVL_ATOMIC_I386_WIN32_VC_HEADER
+#define RAVL_ATOMIC_I386_WIN32_VC_HEADER 1
+//! file="Ravl/Core/Machine/i386-linux-gnu/Atomic.hh"
+//! userlevel=Default
+//! author="Charles Galambos"
+//! date="8/04/2004"
+//! rcsid="$Id$"
+//! docentry="Ravl.Core.Misc"
+//! lib=RavlCore
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct { volatile int counter; } ravl_atomic_t;
+
+#define RAVL_ATOMIC_INIT(i)	{ (i) }
+
+#define ravl_atomic_read(v)		((v)->counter)
+#define ravl_atomic_set(v,i)		(((v)->counter) = (i))
+
+static inline void ravl_atomic_inc(volatile ravl_atomic_t *v) {
+  volatile int &counter = v->counter;
+  __asm {
+    lock inc counter
+  }
+}
+
+static inline void ravl_atomic_dec(volatile ravl_atomic_t *v) {
+  volatile int &counter = v->counter;
+  __asm {
+    lock dec counter
+  }
+}
+
+static inline int ravl_atomic_dec_and_test(volatile ravl_atomic_t *v) {
+  unsigned char c;
+  volatile int &counter = v->counter;
+  __asm {
+    lock dec counter
+    sete c
+  }
+  return c != 0;
+}
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
