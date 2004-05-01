f7bc75d76138cd214feb350100ce4b373f3aa175
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2004-05-01 08:49:18 +0000

    Fix typo.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@4286 7226f50c-d813-0410-99bf-979b288ab488

--- Math/Geometry/Euclidean/2D/DelaunayTriangulation2d.hh
+++ Math/Geometry/Euclidean/2D/DelaunayTriangulation2d.hh
@@ -31,8 +31,8 @@ namespace RavlN {
   //!return: Triangulated mesh.
   // The vertices in the created mesh are in same order as the
   // appear in 'points'.
-
-  HEMesh2dC DelaunayTriangulation(const SArray1dC<Point2dC> &points);
+  
+  HEMesh2dC DelaunayTriangulation(const Array1dC<Point2dC> &points);
   //: Create a delaunay triangulation of the given set of points.
   //!param: points - Array of points to triangulate.
   //!return: Triangulated mesh.
