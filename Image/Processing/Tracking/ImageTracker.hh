d1450dbe198d330fe438bc430648c57e255982ce
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2004-08-05 14:34:46 +0000

    Add some doc.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@4497 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Tracking/ImageTracker.hh
+++ Image/Processing/Tracking/ImageTracker.hh
@@ -18,15 +18,23 @@
 
 namespace RavlImageN {
   
-  //: Track 
+  //! userlevel=Normal
+  //: Track an image against a larger mosiac image.
   
   class ImageTrackerC {
   public:
     ImageTrackerC(IntT nPatchSize,IntT aSearchSize,IntT matchThreshold);
     //: Constructor.
+    //!param: patchSize - Size of patches around each corner to match
+    //!param: aSearchSize - Size of search area. i.e. maximum motion to allow for.
+    //!param: matchThreshold - Threshold for accepting a match.
     
     DListC<PairC<Point2dC> > TrackImage(const ImageC<ByteT> &mosaic,const ImageC<ByteT> &data,const Projection2dC &proj);
     //: Track an image in a larger one.
+    //!param: mosiac - Large mosaic image
+    //!param: data - Smaller target image
+    //!param: proj - Estimate of the position of the smaller image.
+    //!return: List of point pairs, one from 'data' image and corresponding point in the 'mosaic'.
     
   protected:
     CornerDetectorSusanC cornerDet;
