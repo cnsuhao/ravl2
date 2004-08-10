4b61becfcb89ac622b2408ce670624669fbefa58
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2004-08-10 20:13:21 +0000

    Fix include guard.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@4507 7226f50c-d813-0410-99bf-979b288ab488

--- PatternRec/Cluster/DesignMeanShiftCluster.hh
+++ PatternRec/Cluster/DesignMeanShiftCluster.hh
@@ -4,8 +4,8 @@
 // General Public License (LGPL). See the lgpl.licence file for details or
 // see http://www.gnu.org/copyleft/lesser.html
 // file-header-ends-here
-#ifndef OMNI_DESIGNMEANSHIFTCLUSTER_HEADER
-#define OMNI_DESIGNMEANSHIFTCLUSTER_HEADER 1
+#ifndef RAVL_DESIGNMEANSHIFTCLUSTER_HEADER
+#define RAVL_DESIGNMEANSHIFTCLUSTER_HEADER 1
 //! author="Charles Galambos"
 //! userlevel=Normal
 //! rcsid="$Id$"
