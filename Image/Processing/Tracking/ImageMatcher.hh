c40407d7cac31b926f8209e705b6312704e73e20
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2004-12-13 16:57:10 +0000

    Added to doc
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@4704 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Tracking/ImageMatcher.hh
+++ Image/Processing/Tracking/ImageMatcher.hh
@@ -42,12 +42,17 @@ namespace RavlImageN {
 
     bool Apply(const ImageC<ByteT> &img, Projection2dC &proj);
     //: Matches an image to the template.
+    //!param: crop[TBLR] - boundaries of <code>img</code> are cropped by this no. of pixels
+    //!param: matchPatchSize, matchSearchSize, matchThreshold - <a href="RavlImageN.ImageTrackerC.html">tracker</a> parameters
+    //!param: zhomogTemplate, zhomogImage - projective scale parameters for imageTemplate and img projective scales
 
     // <code>proj</code> must be supplied in the form of an initial estimate
     // of the transformation (for example that provided by the previous call),
     // which will be updated by this method to the new computed
     // transformation. <br> <code>proj</code> must be in the sense that it
-    // transforms points <i>from</i> the template <i>to</i> <code>img</code>.
+    // transforms points <i>from</i> the template in the constructor <i>to</i>
+    // <code>img</code>.  The new projection will have the projective scales
+    // that were set in the constructor zhomog arguments.
 
   private:
     // stored parameters
--- Image/Processing/Tracking/ImageTracker.hh
+++ Image/Processing/Tracking/ImageTracker.hh
@@ -23,17 +23,17 @@ namespace RavlImageN {
   
   class ImageTrackerC {
   public:
-    ImageTrackerC(IntT nPatchSize,IntT aSearchSize,IntT matchThreshold);
+    ImageTrackerC(IntT PatchSize,IntT SearchSize,IntT matchThreshold);
     //: Constructor.
-    //!param: patchSize - Size of patches around each corner to match
-    //!param: aSearchSize - Size of search area. i.e. maximum motion to allow for.
+    //!param: Patchsize - Size of patches around each corner to match
+    //!param: SearchSize - Size of search area. i.e. maximum motion to allow for.
     //!param: matchThreshold - Threshold for accepting a match.
     
     DListC<PairC<Point2dC> > TrackImage(const ImageC<ByteT> &mosaic,const ImageC<ByteT> &data,const Projection2dC &proj);
     //: Track an image in a larger one.
     //!param: mosiac - Large mosaic image
     //!param: data - Smaller target image
-    //!param: proj - Estimate of the position of the smaller image.
+    //!param: proj - Estimate of the position of the smaller image relative to the larger one.
     //!return: List of point pairs, one from 'data' image and corresponding point in the 'mosaic'.
     
   protected:
