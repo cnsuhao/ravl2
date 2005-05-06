6d594b1b482dcf6cf462b6202d7af4823b8fc524
plugger <plugger@7226f50c-d813-0410-99bf-979b288ab488>
2005-05-06 09:48:35 +0000

    *** empty log message ***
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@4912 7226f50c-d813-0410-99bf-979b288ab488

--- Applications/VGrab/defs.mk
+++ Applications/VGrab/defs.mk
@@ -9,8 +9,13 @@
 DESCRIPTION = VGrab - Video Frame Grabber
 DONOT_SUPPORT=arm
 MAINS = VGrab.cc
-PROGLIBS = RavlCore RavlOS RavlVideoIO RavlImage RavlOSIO \
-RavlDV.opt RavlImgIOV4L.opt RavlExtImgIO.opt RavlDPDisplay \
- RavlImageIO RavlMPEG2.opt CSPDriver.opt RavlURLIO.opt
+
+PROGLIBS=CSPDriver  RavlVideoIO RavlImage RavlOSIO RavlDV.opt RavlImgIOV4L.opt RavlExtImgIO.opt RavlImageIO RavlMPEG2.opt RavlURLIO.opt
+
+#PROGLIBS = RavlCore RavlOS RavlVideoIO RavlImage RavlOSIO \
+#RavlDV.opt RavlImgIOV4L.opt RavlExtImgIO.opt \
+#RavlDPDisplay \
+# RavlImageIO RavlMPEG2.opt CSPDriver.opt \
+#RavlURLIO.opt
 #EHT = Ravl.Applications.VGrab.html 
-USESLIBS = 
+#USESLIBS = 
