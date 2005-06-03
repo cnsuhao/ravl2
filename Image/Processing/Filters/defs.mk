fe15f2255c5ec5f15dec94e14aeae9ab2b1c237c
ktjonsson <ktjonsson@7226f50c-d813-0410-99bf-979b288ab488>
2005-06-03 09:39:34 +0000

    Added WarpMesh2dC.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@4952 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Filters/defs.mk
+++ Image/Processing/Filters/defs.mk
@@ -16,10 +16,10 @@ HEADERS= Convolve2d.hh ConvolveSeparable2d.hh ConvolveHorz2d.hh \
  Matching.hh WarpScale.hh WarpAffine.hh WarpProjective.hh \
  PixelMixer.hh RemoveDistortion.hh WarpThinPlateSpline.hh \
  SpatialDifference.hh  SumRectangles.hh DeinterlaceStream.hh \
- DCT2d.hh ImageExtend.hh ImagePyramid.hh
+ DCT2d.hh ImageExtend.hh ImagePyramid.hh WarpMesh2d.hh
 
 SOURCES= HomomorphicFilter.cc Correlate2d.cc Matching.cc WarpThinPlateSpline.cc \
- GaborFilter2d.cc DCT2d.cc DeinterlaceStream.cc
+ GaborFilter2d.cc DCT2d.cc DeinterlaceStream.cc WarpMesh2d.cc
 
 TESTEXES= testConvolve.cc
 
