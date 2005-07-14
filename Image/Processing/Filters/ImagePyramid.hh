f397339ca8803cbe20b411c593a5023e82f0ecec
omn-crida <omn-crida@7226f50c-d813-0410-99bf-979b288ab488>
2005-07-14 08:29:56 +0000

    Added non-diadic pyramid scaling
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@5021 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Filters/ImagePyramid.hh
+++ Image/Processing/Filters/ImagePyramid.hh
@@ -19,6 +19,7 @@
 #include "Ravl/Collection.hh"
 #include "Ravl/CollectionIter.hh"
 #include "Ravl/Tuple3.hh"
+#include "Ravl/Image/WarpScale.hh"
 
 namespace RavlImageN {
   
@@ -40,6 +41,14 @@ namespace RavlImageN {
     //!param: img - Image to filter.
     //!param: nScales - Number of scales to generate. -1 = Generate a complete pyramid.
     //!param: subSample - If true sub-sample the pixels as well as filtering.
+
+    ImagePyramidC(const ImageC<PixelT> &img,RealT scaleFactor,IntT nScales = 2,bool subSample = false)
+    { ComputeImages(img,scaleFactor,nScales,subSample); }
+    //: Constructor for non-diadic pyramid
+    //!param: img - Image to filer
+    //!param: scaleFactor - the scale factor to apply between levels (2.0 == double size)
+    //!param: nScales - Number of scales to generate. -1 = Generate a complete pyramid.
+    //!param: subSample - If true sub-sample the picels as well as filtering.
     
     void ComputeImages(const ImageC<PixelT> &img,IntT nScales,bool subSample,bool recursive = false);
     //: Compute images over given scales.
@@ -47,6 +56,13 @@ namespace RavlImageN {
     //!param: nScales - Number of scales to generate. -1 = Generate a complete pyramid.
     //!param: subSample - If true sub-sample the pixels as well as filtering.
     //!param: recursive - If true when subsampling, then use the results of previous filtering operations as input for the next.
+
+    void ComputeImages(const ImageC<PixelT> &img,RealT scaleFactor,IntT nScales,bool subSample);
+    //: Compute images over given scales for non-diadic pyramids.
+    //!param: img - Image to filer
+    //!param: scaleFactor - the scale factor to apply between levels (2.0 == double size)
+    //!param: nScales - Number of scales to generate. -1 = Generate a complete pyramid.
+    //!param: subSample - If true sub-sample the picels as well as filtering.
     
     ImageC<PixelT> ScaleImage(const ImageC<PixelT> &img,IntT scale,bool subSample,IntT imgScale = 1);
     //: Compute a scaled image and add it to the pyramid.
@@ -54,6 +70,13 @@ namespace RavlImageN {
     //!param: scale - Scaling to apply.
     //!param: subSample - Sub sample the pixels in the image ?
     //!param: imgScale - Scale of image passed to routine, use 1 if the image at the original scale.
+    //!return: resulting image.
+    
+    ImageC<PixelT> ScaleImage(const ImageC<PixelT> &img,RealT scale,bool subSample);
+    //: Compute a scaled image and add it to the pyramid.
+    //!param: img - Image to filter.
+    //!param: scale - Scaling to apply.
+    //!param: subSample - Sub sample the pixels in the image ?
     //!return: resulting image.
     
     bool Find(RealT reqScale,ImageC<PixelT> &img,RealT &filterScale,RealT &pixelScale,bool notSmaller = false) const;
@@ -101,6 +124,23 @@ namespace RavlImageN {
       }
     }
   }
+
+  template<class PixelT>
+  void ImagePyramidC<PixelT>::ComputeImages(const ImageC<PixelT> &img,RealT scaleFactor,IntT nScales,bool subSample) {
+    images = CollectionC<Tuple3C<RealT,RealT,ImageC<PixelT> > >(nScales+1);
+    images.Insert(Tuple3C<RealT,RealT,ImageC<PixelT> >(1,1,img));
+    IntT minSize = Min(img.Rows(),img.Cols());
+    RealT scale = 1.0;
+    for(int i = 1;(i < nScales) || nScales < 0; i++) {
+      scale *= scaleFactor;
+      IntT kernelSize = (IntT)(scale * 2.0) - 1;
+      if (!(kernelSize & 1))
+        kernelSize++;
+      if(kernelSize >= minSize) // No point in scaling beyond the image size.
+        break;
+      ScaleImage(img,scale,subSample);
+    }
+  }
   
   //: Generate a single filtered image.
   //!param: img - Image to filter.
@@ -131,6 +171,49 @@ namespace RavlImageN {
     for(Array2dIterC<PixelT> sit(ImageC<PixelT>(filteredImage,alignedFrame));sit;sit += scale,dit++)
       *dit = *sit;
     images.Insert(Tuple3C<RealT,RealT,ImageC<PixelT> >(scale * imgScale,scale * imgScale,subImage));
+    return subImage;
+  }
+  
+  //: Generate a single filtered image.
+  //!param: img - Image to filter.
+  //!param: scale - Scaling to apply.
+  //!param: subSample - Sub sample the pixels in the image ?
+  
+  template<class PixelT>
+  ImageC<PixelT> ImagePyramidC<PixelT>::ScaleImage(const ImageC<PixelT> &img,RealT scale,bool subSample) {
+    IntT kernelSize = (IntT)(scale * 2.0) - 1;
+    if (!(kernelSize & 1))
+      kernelSize++;
+    IndexRange2dC outRange = img.Frame();
+    ImageC<PixelT> prepImage;
+    if (((kernelSize - 1) >> 1) > 0)
+      ExtendImageCopy(img,(kernelSize-1) >> 1,prepImage);
+    else
+      prepImage = img;
+    ImageC<PixelT> filteredImage;
+    if (kernelSize > 1) {
+      GaussConvolve2dC<PixelT,PixelT,RealT,PixelT> filter(kernelSize);
+      filteredImage = filter.Apply(prepImage);
+    }
+    else
+      filteredImage = prepImage;
+    if(!subSample) {
+      images.Insert(Tuple3C<RealT,RealT,ImageC<PixelT> >(scale,1.0,filteredImage));
+      return filteredImage;
+    }
+    // FIXME:- This isn't the most efficient way of getting a subsampled image, we could
+    // compute filered values for the points we want in the final image. 
+    //cerr << " Scale=" << scale << "\n";
+    IndexRange2dC alignedFrame;
+    if (((kernelSize-1) >> 1) > 0)
+      alignedFrame = filteredImage.Frame().AlignWithin((kernelSize-1) >> 1);
+    else
+      alignedFrame = filteredImage.Frame();
+    IndexRange2dC subFrame(alignedFrame.Range1().Min() / scale, alignedFrame.Range1().Max() / scale,
+                           alignedFrame.Range2().Min() / scale, alignedFrame.Range2().Max() / scale);
+    WarpScaleC<PixelT> warpScale(subFrame);
+    ImageC<PixelT> subImage = warpScale.Apply(ImageC<PixelT>(filteredImage, alignedFrame));
+    images.Insert(Tuple3C<RealT,RealT,ImageC<PixelT> >(scale,scale,subImage));
     return subImage;
   }
   
