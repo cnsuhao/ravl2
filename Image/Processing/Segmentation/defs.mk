2b0c2e281ef8ceccc46b31e70a5a44ca1ee03ca9
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2005-08-19 11:48:37 +0000

    Use RavlLibFFmpeg if available.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@5088 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Segmentation/defs.mk
+++ Image/Processing/Segmentation/defs.mk
@@ -27,7 +27,7 @@ TESTEXES=testConnectedComponents.cc
 
 USESLIBS=RavlImageProc  
 
-PROGLIBS=RavlImageIO RavlVideoIO RavlMathIO RavlDPDisplay.opt RavlImgIOV4L.opt RavlExtImgIO.opt
+PROGLIBS=RavlImageIO RavlVideoIO RavlMathIO RavlDPDisplay.opt RavlImgIOV4L.opt RavlExtImgIO.opt RavlLibFFmpeg.opt
 
 EXAMPLES=extrema.cc exSegmentation.cc exConnectedComponents.cc 
 
