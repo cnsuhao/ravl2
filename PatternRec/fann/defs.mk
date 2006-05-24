9a0d8d4a1fa81fa5e55d1f2e98211e31056ada2c
kier <kier@7226f50c-d813-0410-99bf-979b288ab488>
2006-05-24 16:32:24 +0000

    put all headers into fann
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@5497 7226f50c-d813-0410-99bf-979b288ab488

--- PatternRec/fann/defs.mk
+++ PatternRec/fann/defs.mk
@@ -1,3 +1,4 @@
-HEADERS=fann.h
+HEADERS=fann.h compat_time.h fann_activation.h fann_error.h fann_io.h floatfann.h fann_cascade.h fann_train.h doublefann.h fann_data.h fann_internal.h fixedfann.h
+
 SOURCES = floatfann.c
 PLIB = fann
