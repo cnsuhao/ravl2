1a2738e35b17507ebfff5409dc305bd1472151ca
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2006-06-19 14:05:44 +0000

    Updated Ravl.Introduction.html and moved it to Core/Doc with the other intro pages
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@5605 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Doc/defs.mk
+++ Core/Doc/defs.mk
@@ -7,7 +7,7 @@
 #! rcsid="$Id$"
 #! file="Ravl/Core/Doc/defs.mk"
 
-EHT= Ravl.Introduction.Debugging.html \
+EHT= Ravl.Introduction.html Ravl.Introduction.Debugging.html \
  Ravl.Introduction.Reference_Counting.html Ravl.Introduction.Naming_Conventions.html \
  Ravl.Introduction.Reference_Counting.Notes.html Ravl.Introduction.Coordinate_Systems.html \
  Ravl.Getting_Started.html  Ravl.Getting_Started.Windows.html Ravl.Install.html
