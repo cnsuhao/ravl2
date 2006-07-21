d15f3607f78d7adbd287c90e51818d907bcde2a2
robowaz <robowaz@7226f50c-d813-0410-99bf-979b288ab488>
2006-07-21 10:39:22 +0000

    Added Swig to config and build order
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@5766 7226f50c-d813-0410-99bf-979b288ab488

--- Contrib/Swig/defs.mk
+++ Contrib/Swig/defs.mk
@@ -4,6 +4,8 @@
 # General Public License (LGPL). See the lgpl.licence file for details or
 # see http://www.gnu.org/copyleft/lesser.html
 
+REQUIRES=Swig
+
 PACKAGE=Ravl/Swig
 
 HEADERS=Types.i Point2d.i Polygon2d.i Image.i IndexRange2d.i Index2d.i Array2d.i Index.i String.i \
