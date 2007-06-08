65bb0977b817589f54d118c43351b8dbc54526d4
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2007-06-08 10:58:18 +0000

    Added copyright
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6189 7226f50c-d813-0410-99bf-979b288ab488

--- Math/Statistics/Histogram/Parzen.hh
+++ Math/Statistics/Histogram/Parzen.hh
@@ -1,3 +1,9 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2002, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 #ifndef RAVL_PARZENWINDOW_HEADER
 #define RAVL_PARZENWINDOW_HEADER 1
 
