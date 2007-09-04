91b06d184eea6a354139b41d3e60a1f27fe405c9
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2007-09-04 10:32:23 +0000

    Fix adjacency texture map for sphere's.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6308 7226f50c-d813-0410-99bf-979b288ab488

--- 3D/Mesh/MeshShapes.cc
+++ 3D/Mesh/MeshShapes.cc
@@ -14,6 +14,12 @@
 
 namespace Ravl3DN {
   
+  static Point2dC TexPnt(RealT v1,RealT v2)
+  { return Point2dC(v1,v2); }
+  
+  static Point3dC World3d(RealT a1,RealT a2,RealT a3)
+  { return Point3dC(a1,a2,a3); }
+  
   //: Create a flat plane
   
   TriMeshC CreateTriMeshPlane(RealT size) {
@@ -21,36 +27,35 @@ namespace Ravl3DN {
     
     SArray1dC<VertexC> vertex(4);
     Vector3dC norm(0,0,1);
-    vertex[0] = VertexC(Point3dC(-hSize,-hSize,0),norm);
-    vertex[1] = VertexC(Point3dC(-hSize, hSize,0),norm);
-    vertex[2] = VertexC(Point3dC( hSize, hSize,0),norm);
-    vertex[3] = VertexC(Point3dC( hSize,-hSize,0),norm);
+    vertex[0] = VertexC(World3d(-hSize,-hSize,0),norm);
+    vertex[1] = VertexC(World3d(-hSize, hSize,0),norm);
+    vertex[2] = VertexC(World3d( hSize, hSize,0),norm);
+    vertex[3] = VertexC(World3d( hSize,-hSize,0),norm);
     
     SArray1dC<TriC> tri(2);
-    tri[0] = TriC(vertex[0],vertex[2],vertex[1],Point2dC(0,0),Point2dC(1,1),Point2dC(0,1));
-    tri[1] = TriC(vertex[0],vertex[3],vertex[2],Point2dC(0,0),Point2dC(1,0),Point2dC(1,1));
+    tri[0] = TriC(vertex[0],vertex[2],vertex[1],TexPnt(0,0),TexPnt(1,1),TexPnt(0,1));
+    tri[1] = TriC(vertex[0],vertex[3],vertex[2],TexPnt(0,0),TexPnt(1,0),TexPnt(1,1));
     
     tri[0].SetFaceNormal(norm);
     tri[1].SetFaceNormal(norm);
     
     return TriMeshC(vertex,tri,true);
   }
-  
-  
+
   // Create a cube.
   
   TriMeshC CreateTriMeshCube(RealT size) {
     RealT hSize = size/2.0;
     SArray1dC<VertexC> vertex(8);
-    vertex[0] = VertexC(Point3dC( hSize, hSize,-hSize));
-    vertex[1] = VertexC(Point3dC( hSize,-hSize,-hSize));
-    vertex[2] = VertexC(Point3dC(-hSize,-hSize,-hSize));
-    vertex[3] = VertexC(Point3dC(-hSize, hSize,-hSize));
+    vertex[0] = VertexC(World3d( hSize, hSize,-hSize));
+    vertex[1] = VertexC(World3d( hSize,-hSize,-hSize));
+    vertex[2] = VertexC(World3d(-hSize,-hSize,-hSize));
+    vertex[3] = VertexC(World3d(-hSize, hSize,-hSize));
     
-    vertex[4] = VertexC(Point3dC( hSize, hSize,hSize));
-    vertex[5] = VertexC(Point3dC( hSize,-hSize,hSize));
-    vertex[6] = VertexC(Point3dC(-hSize,-hSize,hSize));
-    vertex[7] = VertexC(Point3dC(-hSize, hSize,hSize));
+    vertex[4] = VertexC(World3d( hSize, hSize,hSize));
+    vertex[5] = VertexC(World3d( hSize,-hSize,hSize));
+    vertex[6] = VertexC(World3d(-hSize,-hSize,hSize));
+    vertex[7] = VertexC(World3d(-hSize, hSize,hSize));
     
     SArray1dC<TriC> tri(12);
     UIntT tn = 0;
@@ -70,28 +75,28 @@ namespace Ravl3DN {
     RealT tc5 = 1.0 - sep;
     
     // Front
-    tri[tn++] = TriC(vertex[0],vertex[1],vertex[2],Point2dC(tr0,tc0),Point2dC(tr0,tc1),Point2dC(tr1,tc1));
-    tri[tn++] = TriC(vertex[2],vertex[3],vertex[0],Point2dC(tr1,tc1),Point2dC(tr1,tc0),Point2dC(tr0,tc0));
+    tri[tn++] = TriC(vertex[0],vertex[1],vertex[2],TexPnt(tr0,tc0),TexPnt(tr0,tc1),TexPnt(tr1,tc1));
+    tri[tn++] = TriC(vertex[2],vertex[3],vertex[0],TexPnt(tr1,tc1),TexPnt(tr1,tc0),TexPnt(tr0,tc0));
     
     // Back
-    tri[tn++] = TriC(vertex[6],vertex[5],vertex[4],Point2dC(tr2,tc0),Point2dC(tr2,tc1),Point2dC(tr3,tc1));
-    tri[tn++] = TriC(vertex[4],vertex[7],vertex[6],Point2dC(tr3,tc1),Point2dC(tr3,tc0),Point2dC(tr2,tc0));
+    tri[tn++] = TriC(vertex[6],vertex[5],vertex[4],TexPnt(tr2,tc0),TexPnt(tr2,tc1),TexPnt(tr3,tc1));
+    tri[tn++] = TriC(vertex[4],vertex[7],vertex[6],TexPnt(tr3,tc1),TexPnt(tr3,tc0),TexPnt(tr2,tc0));
     
     // Top
-    tri[tn++] = TriC(vertex[4],vertex[5],vertex[1],Point2dC(tr0,tc2),Point2dC(tr0,tc3),Point2dC(tr1,tc3));
-    tri[tn++] = TriC(vertex[0],vertex[4],vertex[1],Point2dC(tr1,tc2),Point2dC(tr0,tc2),Point2dC(tr1,tc3));
+    tri[tn++] = TriC(vertex[4],vertex[5],vertex[1],TexPnt(tr0,tc2),TexPnt(tr0,tc3),TexPnt(tr1,tc3));
+    tri[tn++] = TriC(vertex[0],vertex[4],vertex[1],TexPnt(tr1,tc2),TexPnt(tr0,tc2),TexPnt(tr1,tc3));
     
     // Bottom
-    tri[tn++] = TriC(vertex[7],vertex[3],vertex[2],Point2dC(tr2,tc2),Point2dC(tr3,tc2),Point2dC(tr3,tc3));
-    tri[tn++] = TriC(vertex[2],vertex[6],vertex[7],Point2dC(tr3,tc3),Point2dC(tr2,tc3),Point2dC(tr2,tc2));
+    tri[tn++] = TriC(vertex[7],vertex[3],vertex[2],TexPnt(tr2,tc2),TexPnt(tr3,tc2),TexPnt(tr3,tc3));
+    tri[tn++] = TriC(vertex[2],vertex[6],vertex[7],TexPnt(tr3,tc3),TexPnt(tr2,tc3),TexPnt(tr2,tc2));
     
     // Left
-    tri[tn++] = TriC(vertex[4],vertex[0],vertex[7],Point2dC(tr0,tc4),Point2dC(tr0,tc5),Point2dC(tr1,tc4));
-    tri[tn++] = TriC(vertex[0],vertex[3],vertex[7],Point2dC(tr0,tc5),Point2dC(tr1,tc5),Point2dC(tr1,tc4));
+    tri[tn++] = TriC(vertex[4],vertex[0],vertex[7],TexPnt(tr0,tc4),TexPnt(tr0,tc5),TexPnt(tr1,tc4));
+    tri[tn++] = TriC(vertex[0],vertex[3],vertex[7],TexPnt(tr0,tc5),TexPnt(tr1,tc5),TexPnt(tr1,tc4));
     
     // Right
-    tri[tn++] = TriC(vertex[1],vertex[5],vertex[2],Point2dC(tr2,tc5),Point2dC(tr2,tc4),Point2dC(tr3,tc5));
-    tri[tn++] = TriC(vertex[5],vertex[6],vertex[2],Point2dC(tr2,tc4),Point2dC(tr3,tc4),Point2dC(tr3,tc5));
+    tri[tn++] = TriC(vertex[1],vertex[5],vertex[2],TexPnt(tr2,tc5),TexPnt(tr2,tc4),TexPnt(tr3,tc5));
+    tri[tn++] = TriC(vertex[5],vertex[6],vertex[2],TexPnt(tr2,tc4),TexPnt(tr3,tc4),TexPnt(tr3,tc5));
     
     TriMeshC ret(vertex,tri,true);
     ret.UpdateVertexNormals();
@@ -137,7 +142,7 @@ namespace Ravl3DN {
     // ----- Put in top fan. -----
     
     IntT topVert = vn;
-    vertex[vn++] = VertexC(Point3dC(0,radius,0));
+    vertex[vn++] = VertexC(World3d(0,radius,0));
     
     RealT layerAngle = layerStep;
     
@@ -145,7 +150,7 @@ namespace Ravl3DN {
     RealT ls = Sin(layerAngle);
     
     vLastLayer[0] = vn;
-    vertex[vn++] = VertexC(Point3dC(ls * sliceSin[0],lc,ls  * sliceCos[0]));
+    vertex[vn++] = VertexC(World3d(ls * sliceSin[0],lc,ls  * sliceCos[0]));
     
     RealT texRow0 = 0;
     RealT texRow1 = texRowSize;
@@ -158,17 +163,17 @@ namespace Ravl3DN {
       texCol1 = texCol0 + texColSize;
       
       tri[tn++] = TriC(vertex[topVert],vertex[vn-1],vertex[vn],
-                       Point2dC(texRow0,texCol0 + texColSize/2.0),Point2dC(texRow1,texCol0),Point2dC(texRow1,texCol1));
+                       TexPnt(texRow0,texCol0 + texColSize/2.0),TexPnt(texRow1,texCol0),TexPnt(texRow1,texCol1));
       
       vLastLayer[s] = vn;
-      vertex[vn++] = VertexC(Point3dC(ls * sliceSin[s],lc,ls  * sliceCos[s]));
+      vertex[vn++] = VertexC(World3d(ls * sliceSin[s],lc,ls  * sliceCos[s]));
     }
     
     texCol0 = 0;
     texCol1 = texColSize;
     
     tri[tn++] = TriC(vertex[topVert],vertex[vn-1],vertex[vLastLayer[0]],
-                     Point2dC(texRow0,texCol0 + texColSize/2.0),Point2dC(texRow1,texCol0),Point2dC(texRow1,texCol1)
+                     TexPnt(texRow0,texCol0 + texColSize/2.0),TexPnt(texRow1,texCol0),TexPnt(texRow1,texCol1)
                      );
     
     
@@ -182,7 +187,7 @@ namespace Ravl3DN {
       UIntT lastTopVert = vLastLayer[0];
       UIntT lastBotVert = vn;
       UIntT firstVirt = vn;
-      vertex[vn++] = VertexC(Point3dC(ls * sliceSin[0],lc,ls  * sliceCos[0]));
+      vertex[vn++] = VertexC(World3d(ls * sliceSin[0],lc,ls  * sliceCos[0]));
       
       RealT texRow0 = l * texRowSize;
       RealT texRow1 = texRow0 + texRowSize;
@@ -193,16 +198,16 @@ namespace Ravl3DN {
         
 	// Put in face triangles.
         tri[tn++] = TriC(vertex[vn],vertex[lastTopVert],vertex[lastBotVert],
-                         Point2dC(texRow1,texCol1),Point2dC(texRow0,texCol0),Point2dC(texRow1,texCol0));
+                         TexPnt(texRow1,texCol1),TexPnt(texRow0,texCol0),TexPnt(texRow1,texCol0));
         
         tri[tn++] = TriC(vertex[vLastLayer[s]],vertex[lastTopVert],vertex[vn],
-                         Point2dC(texRow0,texCol1),Point2dC(texRow0,texCol0),Point2dC(texRow1,texCol1));
+                         TexPnt(texRow0,texCol1),TexPnt(texRow0,texCol0),TexPnt(texRow1,texCol1));
         
 	// Put in new vertex.
 	lastTopVert = vLastLayer[s];
 	lastBotVert = vn;
 	vLastLayer[s] = vn;
-	vertex[vn++] = VertexC(Point3dC(ls * sliceSin[s],lc,ls  * sliceCos[s]));
+	vertex[vn++] = VertexC(World3d(ls * sliceSin[s],lc,ls  * sliceCos[s]));
       }
       
       texCol0 = 0;
@@ -210,10 +215,10 @@ namespace Ravl3DN {
       
       // Put in final face triangles.
       tri[tn++] = TriC(vertex[firstVirt],vertex[lastTopVert],vertex[lastBotVert],
-                       Point2dC(texRow1,texCol1),Point2dC(texRow0,texCol0),Point2dC(texRow1,texCol0));
+                       TexPnt(texRow1,texCol1),TexPnt(texRow0,texCol0),TexPnt(texRow1,texCol0));
       
       tri[tn++] = TriC(vertex[vLastLayer[0]],vertex[lastTopVert],vertex[firstVirt],
-                       Point2dC(texRow0,texCol1),Point2dC(texRow0,texCol0),Point2dC(texRow1,texCol1));
+                       TexPnt(texRow0,texCol1),TexPnt(texRow0,texCol0),TexPnt(texRow1,texCol1));
       
       vLastLayer[0] = firstVirt;
     }
@@ -221,7 +226,7 @@ namespace Ravl3DN {
     // ---- Put in bottom fan ----
     
     IntT botVert = vn;
-    vertex[vn++] = VertexC(Point3dC(0,-radius,0));
+    vertex[vn++] = VertexC(World3d(0,-radius,0));
     
     // Put together face.
     texRow0 = 1.0-texRowSize;
@@ -230,8 +235,12 @@ namespace Ravl3DN {
     UIntT nextVert = vLastLayer[0];
     texCol0 = 0;
     texCol1 = texColSize;
-    tri[tn++] = TriC(vertex[vLastLayer[slices-1]],vertex[botVert],vertex[nextVert],
-                     Point2dC(texRow0,texCol1),Point2dC(texRow0,texCol0),Point2dC(texRow1,texCol0 + texColSize/2.0));
+
+    //tri[tn++] = TriC(vertex[vLastLayer[slices-1]],vertex[botVert],vertex[nextVert],
+    //                 TexPnt(texRow0,texCol1),TexPnt(texRow0,texCol0),TexPnt(texRow1,texCol0 + texColSize/2.0));
+
+    tri[tn++] = TriC(vertex[nextVert],vertex[vLastLayer[slices-1]],vertex[botVert],
+                     TexPnt(texRow0,texCol1),TexPnt(texRow0,texCol0),TexPnt(texRow1,texCol0 + texColSize/2.0));
     
     UIntT lastVert = nextVert;
     
@@ -243,8 +252,8 @@ namespace Ravl3DN {
       texCol0 = texColSize * s;
       texCol1 = texCol0 + texColSize;
       
-      tri[tn++] = TriC(vertex[lastVert],vertex[botVert],vertex[nextVert],
-                       Point2dC(texRow0,texCol1),Point2dC(texRow0,texCol0),Point2dC(texRow1,texCol0 + texColSize/2.0));
+      tri[tn++] = TriC(vertex[nextVert],vertex[lastVert],vertex[botVert],
+                       TexPnt(texRow0,texCol1),TexPnt(texRow0,texCol0),TexPnt(texRow1,texCol0 + texColSize/2.0));
       
       lastVert = nextVert;
     }
