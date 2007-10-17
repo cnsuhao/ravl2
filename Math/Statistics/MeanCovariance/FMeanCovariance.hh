af3d62abe7b72a3103af94cd08bd46acf7d19272
robowaz <robowaz@7226f50c-d813-0410-99bf-979b288ab488>
2007-10-17 09:04:40 +0000

    Fixed saving to binary stream (Automatic whitespace cleanup)
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6397 7226f50c-d813-0410-99bf-979b288ab488

--- Math/Statistics/MeanCovariance/FMeanCovariance.hh
+++ Math/Statistics/MeanCovariance/FMeanCovariance.hh
@@ -1,4 +1,4 @@
-// This file is part of RAVL, Recognition And Vision Library 
+// This file is part of RAVL, Recognition And Vision Library
 // Copyright (C) 2002, University of Surrey
 // This code may be redistributed under the terms of the GNU Lesser
 // General Public License (LGPL). See the lgpl.licence file for details or
@@ -24,63 +24,63 @@ namespace RavlN {
   template<unsigned int N> class FMeanCovarianceC ;
   template<unsigned int N> istream & operator>>(istream & inS, FMeanCovarianceC<N> & meanCov);
   template<unsigned int N> BinIStreamC & operator>>(BinIStreamC & inS, FMeanCovarianceC<N> & meanCov);
-  
+
   //! userlevel=Normal
-  //: Mean and covariance together in N-D space 
+  //: Mean and covariance together in N-D space
   // The class FMeanCovarianceC serves for computation of the mean
   // and the covariance matrix of a set of fixed dimensional data points.
-  
+
   template<unsigned int N>
   class FMeanCovarianceC {
-  public:    
+  public:
     FMeanCovarianceC<N>()
     { cov.Fill(0); }
     //: Constructor.
     // Creates zero mean and zero covariance matrix representing
     // the fixed-dimensional set containing no data points.
-    
+
     FMeanCovarianceC(const FVectorC<N> & point)
       : m(point)
     { cov.Fill(0); }
-    //: Constructor 
+    //: Constructor
     // Creates the mean vector and zero covariance matrix representing
     // the data set containing just one data point.
-    
+
     FMeanCovarianceC(const FMeanC<N> & mean)
       : m(mean)
     { cov.Fill(0); }
     // Creates the mean vector and zero covariance matrix representing
     // the data set represented by the 'mean'.
-    
-    FMeanCovarianceC(SizeT  n, 
-		     const FVectorC<N> & mean, 
-		     const FMatrixC<N,N> & ncov)
-      : m(n,mean), 
-	cov(ncov)
+
+    FMeanCovarianceC(SizeT  n,
+                     const FVectorC<N> & mean,
+                     const FMatrixC<N,N> & ncov)
+      : m(n,mean),
+        cov(ncov)
     {}
     // Creates the mean vector and zero covariance matrix representing
     // the data set containing 'n' points and represented by the 'mean'
     // and the covariance matrix 'cov'.
-    
+
     FMeanCovarianceC<N> Copy() const;
     //: Returns a new physical copy of this object.
-    
+
     FMeanCovarianceC<N>(const SArray1dC<FVectorC<N> > & data);
     //: Compute the mean and covariance of an array of vectors.
-    
+
     //:---------------------------
     // Information about an object
 
     SizeT Number() const
       { return m.Number(); }
     //: Returns the number of data points which are represented by this object.
-    
+
     const FVectorC<N> & Mean() const
     { return m.Mean(); }
     //: Access the mean.
     // Returns the mean vector of data points which are represented
     // by this object.
-    
+
     const FMatrixC<N,N> & Covariance() const
     { return cov; }
     //: Access the covariance.
@@ -88,8 +88,8 @@ namespace RavlN {
     // by this object.
 
     // Object modification
-    // -------------------      
-    
+    // -------------------
+
     const FMeanCovarianceC<N> & SetZero();
     //: Total initialization of this object resulting in the representation
     //: the empty set of data points.
@@ -98,31 +98,31 @@ namespace RavlN {
     //: Adds one point to the set of data points.
 
     const FMeanCovarianceC<N> & operator-=(const FVectorC<N> & point);
-    //: Removes one point from the set of data points. 
-    // Be carefull to remove a point which was already added to the set, 
+    //: Removes one point from the set of data points.
+    // Be carefull to remove a point which was already added to the set,
     // otherwise the representation will not describe a real set.
 
     const FMeanCovarianceC<N> & operator+=(const FMeanC<N> & mean);
     //: Adds a number of data poits represented by the 'mean' and zero
     //: covariance matrix to this set.
-    
+
     const FMeanCovarianceC<N> & operator-=(const FMeanC<N> & mean);
     //: Removes a number of data poits represented by the 'mean' and
-    //: zero covariance matrix from this set. 
+    //: zero covariance matrix from this set.
     // Be carefull to remove
     // points which were already added to the set, otherwise the representation
     // will not describe a real set.
-    
+
     const FMeanCovarianceC<N> & operator+=(const FMeanCovarianceC<N> & meanCov);
     //: Adds a number of data points represented by the 'meanCov' structure
     //: to this set.
 
     const FMeanCovarianceC<N> & operator-=(const FMeanCovarianceC<N> & meanCov);
     //: Removes a number of data points represented by the 'meanCov' structure
-    //: from this set. 
-    // Be carefull to remove points which were already added to the set, otherwise 
+    //: from this set.
+    // Be carefull to remove points which were already added to the set, otherwise
     // the representation will not describe a real set.
-    
+
     const FMeanCovarianceC<N> & Add(const FVectorC<N> & point, const FVectorC<N> & var);
     // Updates the mean and the covariance matrix by adding one N-d point
     // whose coordinates are known with the error described by the diagonal
@@ -137,43 +137,46 @@ namespace RavlN {
     // will not describe a real set.
 
     const FMeanCovarianceC<N> & SetSum(const FMeanCovarianceC<N> & meanCov1,
-				       const FMeanCovarianceC<N> & meanCov2);
+               const FMeanCovarianceC<N> & meanCov2);
     //: This object is set to be the union of two set of data points 'meanCov1'
     //: and 'meanCov2'.
 
     FMeanCovarianceC<N> operator*(const FMeanCovarianceC<N> &oth) const;
     //: Calculate the product of the two probability density functions.
     // This assumes the estimates of the distributions are accurate. (The number
-    // of samples is ignored) 
-    
+    // of samples is ignored)
+
     RealT Gauss(const FVectorC<N> & point) const;
     //: Evaulate value of normal distribution at given point.
-    
+
+    bool Save(BinOStreamC &outS) const;
+    //: Save the object to a binary stream.
+
   protected:
     FMeanC<N> m;   // The mean vector of this data set.
     FMatrixC<N,N> cov; // the covariance matrix of this data set.
-    
+
     friend istream & operator>> <>(istream & inS, FMeanCovarianceC<N> & meanCov);
     friend BinIStreamC & operator>> <>(BinIStreamC & inS, FMeanCovarianceC<N> & meanCov);
   };
-  
+
   template<unsigned int N>
   ostream & operator<<(ostream & outS, const FMeanCovarianceC<N> & meanCov) {
-    outS << meanCov.Mean() << '\n' 
-	 << meanCov.Covariance() << '\n';
+    outS << meanCov.Mean() << '\n'
+    << meanCov.Covariance() << '\n';
     return outS;
   }
   // Saves the statistical description of the set 'meanCov' into the output
   // stream 'outS'.
-  
+
   template<unsigned int N>
   BinOStreamC & operator<<(BinOStreamC & outS, const FMeanCovarianceC<N> & meanCov) {
-    outS << meanCov.Mean() << meanCov.Covariance();
+    meanCov.Save(outS);
     return outS;
   }
   // Saves the statistical description of the set 'meanCov' into the output
   // stream 'outS'.
-  
+
   template<unsigned int N>
   istream & operator>>(istream & inS, FMeanCovarianceC<N> & meanCov) {
     inS >> meanCov.m >> meanCov.cov;
@@ -181,7 +184,7 @@ namespace RavlN {
   }
   // Reads and sets the statistical description of the set 'meanCov'
   // according to the information in the input stream 'inS'.
-  
+
   template<unsigned int N>
   BinIStreamC & operator>>(BinIStreamC & inS, FMeanCovarianceC<N> & meanCov) {
     inS >> meanCov.m >> meanCov.cov;
@@ -189,11 +192,11 @@ namespace RavlN {
   }
   // Reads and sets the statistical description of the set 'meanCov'
   // according to the information in the input stream 'inS'.
-  
+
   //: Compute the mean and covariance of an array of vectors.
-  
+
   template<unsigned int N>
-  FMeanCovarianceC<N>::FMeanCovarianceC(const SArray1dC<FVectorC<N> > & data) 
+  FMeanCovarianceC<N>::FMeanCovarianceC(const SArray1dC<FVectorC<N> > & data)
     : m(data)
   {
     if(Number() == 0)
@@ -207,14 +210,14 @@ namespace RavlN {
     cov /= (RealT) Number();
     cov -= Mean().OuterProduct(tmp);
   }
-  
+
   template<unsigned int N>
   const FMeanCovarianceC<N> & FMeanCovarianceC<N>::SetZero() {
     m.Fill(0);
     cov.Fill(0);
     return *this;
   }
-  
+
   template<unsigned int N>
   const FMeanCovarianceC<N> &FMeanCovarianceC<N>::operator+=(const FVectorC<N> & point) {
     // Update the covariance matrix.
@@ -224,13 +227,13 @@ namespace RavlN {
     FMatrixC<N,N> tmp;
     cov *= p1;
     cov += (FVectorC<N>(point - Mean()).OuterProduct(tmp) *= p2);
-    
+
     // Update the mean.
     m += point;
-    
+
     return *this;
   }
-  
+
   template<unsigned int N>
   const FMeanCovarianceC<N> & FMeanCovarianceC<N>::operator-=(const FVectorC<N> & point) {
     const RealT number = (RealT) Number();
@@ -244,13 +247,13 @@ namespace RavlN {
       const RealT p2 = p1 / nDen;
       cov *= p1;
       cov -= (FVectorC<N>(point - Mean()).OuterProduct(tmp) *= p2);
-      
+
       // Update the mean.
       m -= point;
     }
     return *this;
   }
-  
+
   template<unsigned int N>
   const FMeanCovarianceC<N> & FMeanCovarianceC<N>::operator+=(const FMeanC<N> & mean) {
     // Update the covariance matrix.
@@ -265,19 +268,19 @@ namespace RavlN {
       const RealT p2 = number2 / nDen;
       cov *= p1;
       cov += (FVectorC<N>(mean.Mean() - Mean()).OuterProduct(tmp) *= p1*p2);
-      
+
       // Update the mean.
       m += mean;
     }
     return *this;
   }
-  
+
   template<unsigned int N>
   const FMeanCovarianceC<N> & FMeanCovarianceC<N>::operator-=(const FMeanC<N> & mean) {
     const RealT number1 = (RealT) Number();
     const RealT number2 = (RealT) mean.Number();
     const RealT nDen    = number1 - number2;
-    if (nDen <= 0) { 
+    if (nDen <= 0) {
       SetZero();
     } else  {
       // Update the covariance matrix.
@@ -286,13 +289,13 @@ namespace RavlN {
       const RealT p2 = number2 / nDen;
       cov *= p1;
       cov -= (FVectorC<N>(mean.Mean() - Mean()).OuterProduct(tmp) *= p1*p2);
-      
+
       // Update the mean.
       m -= mean;
     }
     return *this;
   }
-  
+
   template<unsigned int N>
   const FMeanCovarianceC<N> & FMeanCovarianceC<N>::operator+=(const FMeanCovarianceC<N> & meanCov) {
     // Update the covariance matrix.
@@ -306,17 +309,17 @@ namespace RavlN {
       const RealT p1 = number1 / nDen;
       const RealT p2 = number2 / nDen;
       if (&cov != &(meanCov.Covariance())) {
-	cov *= p1;
-	cov += meanCov.Covariance() * p2;
+        cov *= p1;
+        cov += meanCov.Covariance() * p2;
       }
       cov += (FVectorC<N>(meanCov.Mean() - Mean()).OuterProduct(tmp) *= p1*p2);
-      
+
       // Update the mean.
       m += meanCov.m;
     }
     return *this;
   }
-  
+
   template<unsigned int N>
   const FMeanCovarianceC<N> & FMeanCovarianceC<N>::operator-=(const FMeanCovarianceC<N> & meanCov) {
     const RealT number1 = (RealT) Number();
@@ -330,17 +333,17 @@ namespace RavlN {
       const RealT p1 = number1 / nDen;
       const RealT p2 = number2 / nDen;
       if (&cov != &(meanCov.Covariance())) {
-	cov *= p1;
-	cov -= meanCov.Covariance() * p2;
+        cov *= p1;
+        cov -= meanCov.Covariance() * p2;
       }
       cov -= (FVectorC<N>(meanCov.Mean() - Mean()).OuterProduct(tmp) *= p1*p2);
-      
+
       // Update the mean.
       m -= meanCov.m;
     }
     return *this;
   }
-  
+
   template<unsigned int N>
   const FMeanCovarianceC<N> & FMeanCovarianceC<N>::Add(const FVectorC<N> & point, const FVectorC<N> & var) {
     // Update the covariance matrix.
@@ -351,13 +354,13 @@ namespace RavlN {
     cov *= p1;
     cov.AddDiagonal(var * p2);
     cov += (FVectorC<N>(point - Mean()).OuterProduct(tmp) *= p1 * p2);
-    
+
     // Update the mean.
     m += point;
-    
+
     return *this;
   }
-  
+
   template<unsigned int N>
   const FMeanCovarianceC<N> & FMeanCovarianceC<N>::Remove(const FVectorC<N> & point, const FVectorC<N> & var) {
     const RealT number = (RealT) Number();
@@ -372,17 +375,17 @@ namespace RavlN {
       cov *= p1;
       cov.AddDiagonal(var * (-p2));
       cov -= (FVectorC<N>(point - Mean()).OuterProduct(tmp) *= p1*p2);
-      
+
       // Update the mean.
       m -= point;
     }
     return *this;
   }
-  
+
   template<unsigned int N>
-  const FMeanCovarianceC<N> & 
+  const FMeanCovarianceC<N> &
   FMeanCovarianceC<N>::SetSum(const FMeanCovarianceC<N> & meanCov1,
-			      const FMeanCovarianceC<N> & meanCov2) {
+                              const FMeanCovarianceC<N> & meanCov2) {
     *this = meanCov1;
     *this += meanCov2;
     return *this;
@@ -397,8 +400,8 @@ namespace RavlN {
     FMatrixC<N,N> cs = Covariance() * sumCov;
     mean += cs * oth.Mean();
     return FMeanCovarianceC<N>(Number() + oth.Number(),
-			       mean,
-			       cs * oth.Covariance());
+             									 mean,
+                               cs * oth.Covariance());
   }
 
   template<unsigned int N>
@@ -410,7 +413,13 @@ namespace RavlN {
     RealT a = Pow(2 * RavlConstN::pi,(RealT) N / 2) * Sqrt(Determinant(cov)) ;
     return Exp(e)/a;
   }
-  
+
+  template<unsigned int N>
+  bool FMeanCovarianceC<N>::Save(BinOStreamC &outS) const {
+    outS << m << cov;
+    return true;
+  }
+
 }
 #endif
 
