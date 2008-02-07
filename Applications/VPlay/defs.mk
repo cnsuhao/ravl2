82496301cd12c72fc283e458653a7a68d9471ded
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2008-02-07 17:08:50 +0000

    Sort out V4L2.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6548 7226f50c-d813-0410-99bf-979b288ab488

--- Applications/VPlay/defs.mk
+++ Applications/VPlay/defs.mk
@@ -28,10 +28,9 @@ USESLIBS = RavlCore RavlOS RavlImage RavlOSIO RavlGUI RavlDPMT RavlGUI2D RavlGUI
 
 PROGLIBS =  RavlImgIOV4L.opt RavlExtImgIO.opt RavlDPDisplay \
  RavlImageIO RavlVideoIO  CSPDriver.opt RavlURLIO.opt RavlLibMPEG2.opt \
-  RavlImageProc RavlNet RavlDVDRead.opt RavlAVIFile.opt \
- RavlIOV4L2.opt RavlImgIOJasper.opt RavlDV.opt RavlImgIO1394dc.opt \
- RavlLibFFmpeg.opt RavlRawVidIO.opt
-
+ RavlImageProc RavlNet RavlDVDRead.opt RavlAVIFile.opt \
+ RavlImgIOJasper.opt RavlDV.opt  RavlImgIOV4L2.opt \
+ RavlRawVidIO.opt RavlLibFFmpeg.opt RavlImgIO1394dc.opt
 
 #EHT = exeVPlay.eht
 
