80f77e5f6de8dd428e45f295e71e7f4254dd53b6
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2008-02-14 14:24:59 +0000

    Add class for tailing an growing stream.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6570 7226f50c-d813-0410-99bf-979b288ab488

--- OS/DataProc/defs.mk
+++ OS/DataProc/defs.mk
@@ -10,9 +10,10 @@
 PACKAGE=Ravl/DP
 
 HEADERS= MTIOConnect.hh PlayControl.hh ThreadPipe.hh Buffer.hh FixedBuffer.hh \
- Governor.hh Hold.hh SerialisePort.hh Blackboard.hh SPortShare.hh SplitI.hh
+ Governor.hh Hold.hh SerialisePort.hh Blackboard.hh SPortShare.hh SplitI.hh \
+ TailIStream.hh
 
-SOURCES= MTIOConnect.cc PlayControl.cc Governor.cc Blackboard.cc
+SOURCES= MTIOConnect.cc PlayControl.cc Governor.cc Blackboard.cc TailIStream.cc
 
 EXAMPLES= exMTDataProc.cc exDPSplit.cc
 #	exFailOver.cc  exDPMultiplex.cc 
