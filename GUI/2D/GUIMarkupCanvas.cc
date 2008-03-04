9f3a6ce2b675ba98c933a824d74cea24212edf7d
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2008-03-04 11:58:36 +0000

    Tidier version of last checkin
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6647 7226f50c-d813-0410-99bf-979b288ab488

--- GUI/2D/GUIMarkupCanvas.cc
+++ GUI/2D/GUIMarkupCanvas.cc
@@ -309,11 +309,8 @@ namespace RavlGUIN
       {
         updateArea.Involve(m_mouseObj.Extent(*this));
         //Enlarge area by 1 *screen* pixel to eliminate quantising artefacts
-        Vector2dC increment(1.0/scale[0],1.0/scale[1]);
-        updateArea.TRow() -= increment[0]; 
-        updateArea.BRow() += increment[0]; 
-        updateArea.LCol() -= increment[1]; 
-        updateArea.RCol() += increment[1]; 
+        updateArea.RowRange() = updateArea.RowRange().Expand(1.0/scale[0]); 
+        updateArea.ColRange() = updateArea.ColRange().Expand(1.0/scale[1]); 
       } 
       else 
       {
