9da3a91645f6e0a3f3138e1c0518003f8387010f
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2008-03-09 17:39:55 +0000

    Bug fix.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6660 7226f50c-d813-0410-99bf-979b288ab488

--- Math/Geometry/Projective/3D/PPoint3dAffine.cc
+++ Math/Geometry/Projective/3D/PPoint3dAffine.cc
@@ -15,10 +15,10 @@ namespace RavlN {
   PPoint3dC Transform(const FAffineC<3> &transform,const PPoint3dC & p) {
     const FMatrixC<3,3> &sr = transform.SRMatrix();
     const FVectorC<3> &t =  transform.Translation();
-    return PPoint3dC(sr[0][0] * p[0] + sr[0][1] * p[1] + sr[0][2] * p[2] + t[0] * p[4],
-                     sr[1][0] * p[0] + sr[1][1] * p[1] + sr[1][2] * p[2] + t[1] * p[4],
-                     sr[2][0] * p[0] + sr[2][1] * p[1] + sr[2][2] * p[2] + t[2] * p[4],
-                     p[4]);
+    return PPoint3dC(sr[0][0] * p[0] + sr[0][1] * p[1] + sr[0][2] * p[2] + t[0] * p[3],
+                     sr[1][0] * p[0] + sr[1][1] * p[1] + sr[1][2] * p[2] + t[1] * p[3],
+                     sr[2][0] * p[0] + sr[2][1] * p[1] + sr[2][2] * p[2] + t[2] * p[3],
+                     p[3]);
     
   }
   
