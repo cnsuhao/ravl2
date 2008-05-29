990337243961ec3023f5be815cb578b67e63ec21
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2008-05-29 17:07:14 +0000

    More user-friendly example
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6817 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Motion/LMSGradient/exLMSOptic.cc
+++ Image/Processing/Motion/LMSGradient/exLMSOptic.cc
@@ -40,14 +40,13 @@ int exLMSOptic(int argc, char **argv)
   // process command-line parameters
   OptionC opt(argc, argv);
   StringC opFileName = opt.String("o", "", "Output file name prefix");
-  StringC opDisplay = opt.String("oi", "@X", "Output file name prefix");
+  StringC opDisplay = opt.String("oi", "@X", "Output display file name prefix");
   IntT windowSize  = opt.Int("w", 9, "Window size");
   IntT order       = opt.Int("g", 5, "Set Gaussian antialias filter order (default is Remex 2:1 antialias)");
   IntT diffOrder   = opt.Int("d", 1, "Spatial gradient difference order");
   RealT noise      = opt.Real("n", 1.0, "Noise s.d. estimate for filtered image");
   //IntT scale       = opt.Int("s", 1, "Scale factor for arrow on Postscript plot");
   //RealT subsample  = opt.Real("S", 5, "Vector subsample factor");
-  bool forward_diff  = opt.Boolean("F", false, "Forward diff instead of average diff for spatial gradient");
   StringC ImageFileName1 = opt.String("", "", "Input image 1");
   StringC ImageFileName2 = opt.String("", "", "Input image 2");
   opt.CompulsoryArgs(2);
@@ -81,19 +80,13 @@ int exLMSOptic(int argc, char **argv)
       filtered[i] = filter.Apply(image[i]);
   }
   
-  // compute image gradients:
-  //   - temporal
-  ImageC<RealT>  dt = (filtered[1]-filtered[0]); 
-  //   - and spatial
-  ImageC<RealT> centre ((forward_diff) ? ((ImageC<RealT> &) filtered[0]) : ImageC<RealT>((filtered[0] + filtered[1]) / 2.0));  
-  ImageC<Vector2dC> grad;
-  SpatialDifference(diffOrder,centre,grad);
   
   // compute motion
   LMSOpticFlowC motion(true);
   motion.SetNoiseLevel(noise);
+  motion.SetGradientOrder(diffOrder);
   motion.SetRegionSize(windowSize);
-  motion.Estimate(grad, dt);
+  motion.Estimate(filtered);
   
   ImageC<ByteYUVValueC> img;
   motion.DrawMotion(image[0],img);
