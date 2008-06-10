e3e0bce518139403736637235c08328382a5c34e
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2008-06-10 16:30:45 +0000

    Rationalised projective warp a bit; added test for it
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6833 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Processing/Filters/Warp/testWarp.cc
+++ Image/Processing/Filters/Warp/testWarp.cc
@@ -1,5 +1,5 @@
 #include "Ravl/Image/WarpAffine.hh"
-
+#include "Ravl/Image/WarpProjective.hh"
 #include "Ravl/Image/Image.hh"
 #include "Ravl/Index2d.hh"
 #include "Ravl/IO.hh"
@@ -8,11 +8,16 @@ using namespace RavlN;
 using namespace RavlImageN;
 
 int testWarpAffine();
+int testWarpProjective();
 
 
 int main () {
   int ln;
   if((ln = testWarpAffine()) != 0) {
+    cerr << "Test failed on line " << ln << "\n";
+    return 1;
+  }
+  if((ln = testWarpProjective()) != 0) {
     cerr << "Test failed on line " << ln << "\n";
     return 1;
   }
@@ -26,7 +31,7 @@ int testWarpAffine() {
   ImageC<RealT> out;
   Affine2dC M;
 
-  //unit transformation; origin @ t.l. of pixel
+  //unit transformation; origin @ t.l. of pixel (default)
   M  = Affine2dC(Vector2dC(1,1),0,Vector2dC(0,0)); 
   WarpAffineC<RealT> warp(im.Frame().Expand(-1), M);
   out = warp.Apply(im);
@@ -37,18 +42,58 @@ int testWarpAffine() {
   out = warp.Apply(im);
   for (Array2dIter2C<RealT,RealT>i(out,im,out.Frame()); i; ++i) if (Abs(i.Data1()-i.Data2()) > 1.0e-12)  return __LINE__;
 
-  // rotate by +pi/2
+  // rotate by +pi/2; origin @ t.l. of pixel
   M  = Affine2dC(Vector2dC(1,1),RavlConstN::pi/2,Vector2dC(0,0)); //rotate by pi/2
   warp = WarpAffineC<RealT>(im.Frame().Expand(-1), M);
   warp.SetMidPixelCorrection(true);
   out = warp.Apply(im);
   if (Abs(out[2][-2] - 1) > 1.0e-12)  return __LINE__;
 
-  // rotate by +pi/2
+  // rotate by +pi/2; origin @ centre of pixel
   M  = Affine2dC(Vector2dC(1,1),RavlConstN::pi/2,Vector2dC(0,0)); //rotate by pi/2
   warp = WarpAffineC<RealT>(im.Frame().Expand(-1), M);
   warp.SetMidPixelCorrection(false);
   out = warp.Apply(im);
+  if (Abs(out[2][-1] - 1) > 1.0e-12)  return __LINE__;
+
+  return 0;
+}
+
+
+int testWarpProjective() {
+  ImageC<RealT> im(ImageRectangleC(Index2dC(0,0), 8), 0);
+  im[0][0] = 1;
+  im[1][2] = 1;
+  ImageC<RealT> out;
+  Projection2dC M;
+
+  //unit transformation; origin @ t.l. of pixel (default)
+  M  = Projection2dC(Affine2dC(Vector2dC(1,1),0,Vector2dC(0,0))); 
+  WarpProjectiveC<RealT> warp(im.Frame().Expand(-1), M);
+  out = warp.Apply(im);
+  for (Array2dIter2C<RealT,RealT>i(out,im,out.Frame()); i; ++i) if (Abs(i.Data1()-i.Data2()) > 1.0e-12)  return __LINE__;
+
+  //unit transformation; origin @ centre of pixel
+  warp.SetMidPixelCorrection(false);
+  out = warp.Apply(im);
+  for (Array2dIter2C<RealT,RealT>i(out,im,out.Frame()); i; ++i) if (Abs(i.Data1()-i.Data2()) > 1.0e-12)  return __LINE__;
+
+  // ***Ideally this test should be the same as for affine warp.***  
+  // ***But currently there is an interface inconsistency.***
+  // rotate by -pi/2; origin @ t.l. of pixel
+  M  = Projection2dC(Affine2dC(Vector2dC(1,1),-RavlConstN::pi/2,Vector2dC(0,0))); //rotate by pi/2
+  warp = WarpProjectiveC<RealT>(im.Frame().Expand(-1), M);
+  warp.SetMidPixelCorrection(true);
+  out = warp.Apply(im);
+  //cout <<im<<endl<<out<<endl; 
+  if (Abs(out[2][-2] - 1) > 1.0e-12)  return __LINE__;
+
+  // rotate by -pi/2; origin @ centre of pixel
+  M  = Projection2dC(Affine2dC(Vector2dC(1,1),-RavlConstN::pi/2,Vector2dC(0,0))); //rotate by pi/2
+  warp = WarpProjectiveC<RealT>(im.Frame().Expand(-1), M);
+  warp.SetMidPixelCorrection(false);
+  out = warp.Apply(im);
+  //cout <<out<<endl; 
   if (Abs(out[2][-1] - 1) > 1.0e-12)  return __LINE__;
 
   return 0;
