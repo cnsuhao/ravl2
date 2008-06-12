196405d588cbec2aa0cd2f3055d6d5689f3627d4
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2008-06-12 16:36:27 +0000

    Add float method.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6837 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Base/VectorUtils.cc
+++ Core/Base/VectorUtils.cc
@@ -5,7 +5,7 @@ namespace RavlBaseVectorN {
 
   using namespace RavlN;
   
-  static double BaseDotProduct(const double* v1, const double* v2, size_t Size) {
+  static double BaseDotProductD(const double* v1, const double* v2, size_t Size) {
     double sum = 0.0;
     const double* ptr1 = v1;
     const double* ptr2 = v2;
@@ -14,6 +14,16 @@ namespace RavlBaseVectorN {
     return sum;  
   }
 
-  double (*pDot)(const double*, const double*, size_t) = &BaseDotProduct;
-
+  static float BaseDotProductF(const float* v1, const float* v2, size_t Size) {
+    float sum = 0.0;
+    const float* ptr1 = v1;
+    const float* ptr2 = v2;
+    for(unsigned int i=Size; i>0; --i)
+      sum += *ptr1++ * *ptr2++;
+    return sum;  
+  }
+  
+  double (*g_DotProductD)(const double*, const double*, size_t) = &BaseDotProductD;
+  float (*g_DotProductF)(const float*, const float*, size_t) = &BaseDotProductF;
+  
 }
--- Core/Base/VectorUtils.hh
+++ Core/Base/VectorUtils.hh
@@ -5,11 +5,15 @@
 
 namespace RavlBaseVectorN {
   
-  extern double (*pDot)(const double*, const double*, size_t) ;
+  extern double (*g_DotProductD)(const double*, const double*, size_t) ;
+  extern float (*g_DotProductF)(const float*, const float*, size_t) ;
+  
+  inline double DotProduct(const double* v1, const double* v2, size_t Size) 
+  { return (*g_DotProductD)(v1,v2,Size); }
 
-  inline double DotProduct(const double* v1, const double* v2, size_t Size) {
-    return (*pDot)(v1,v2,Size);
-  }
+  inline float DotProduct(const float* v1, const float* v2, size_t Size) 
+  { return (*g_DotProductF)(v1,v2,Size); }
+  
 
 }
 
