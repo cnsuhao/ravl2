7b4c9d7794d94082ef6134d3fae92746cf3db677
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2008-06-24 14:02:44 +0000

    Rename some variables and inline function calls.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6862 7226f50c-d813-0410-99bf-979b288ab488

--- Core/I386SSE/VectorUtilsI386SSE.cc
+++ Core/I386SSE/VectorUtilsI386SSE.cc
@@ -4,36 +4,23 @@
 #include "Ravl/CPUID.hh"
 
 namespace RavlBaseVectorN {
-
-  static double BaseDotProductD(const double* v1, const double* v2, size_t Size) {
-    double sum = 0.0;
-    const double* ptr1 = v1;
-    const double* ptr2 = v2;
-    for(unsigned int i=Size; i>0; --i)
-      sum += *ptr1++ * *ptr2++;
-    return sum;  
-  }
-
-  static float BaseDotProductF(const float* v1, const float* v2, size_t Size) {
-    float sum = 0.0;
-    const float* ptr1 = v1;
-    const float* ptr2 = v2;
-    for(unsigned int i=Size; i>0; --i)
-      sum += *ptr1++ * *ptr2++;
-    return sum;  
-  }
   
   using namespace RavlN;
 
   // FIXME:- In both these routines if vectors have the same alignment we could process 
   // the first few entries then process as aligned .
   
-  static double SSEDotProductD(const double* v1, const double* v2, size_t Size) {
-    // For small vectors all the messing about is not worth it.
-    if(Size < 12) return BaseDotProductD(v1,v2,Size);
+  static double SSEDotProductD(const double* v1, const double* v2, size_t size) {
     const double* wPtr = v1;
-    const double* const ewPtr = wPtr + (Size & ~0x1);
     const double* dPtr = v2;
+    if(size < 12) {
+      // For small vectors all the messing about is not worth it.
+      double sum = 0.0;
+      for(unsigned int i=size; i>0; --i)
+        sum += *wPtr++ * *dPtr++;
+      return sum;
+    }
+    const double* const ewPtr = wPtr + (size & ~0x1);
     __m128d sum = _mm_setzero_pd();
     if((((unsigned long int) wPtr) & 0xf) == 0) {  // v1 16-byte aligned ?
       if((((unsigned long int) dPtr) & 0xf) == 0) {// v2 16-byte aligned ?
@@ -68,7 +55,7 @@ namespace RavlBaseVectorN {
       }
     }
 
-    if(Size & 1) { // Odd length ?
+    if(size & 1) { // Odd length ?
       sum = _mm_add_pd(sum, _mm_mul_sd(_mm_load_sd(dPtr++), _mm_load_sd(wPtr++)));
     }
     double tmp[2];
@@ -76,15 +63,18 @@ namespace RavlBaseVectorN {
     return tmp[0] + tmp[1];
   }
   
-  
-  
   static float SSEDotProductF(const float *v1,const float *v2,size_t n) {
-    // Not worth using this code for small vectors.
-    if(n < 12) return BaseDotProductF(v1,v2,n);
     const float* wPtr = v1;
+    const float* dPtr = v2;
+    if(n < 12) {
+      // Not worth using this code for small vectors.
+      float sum = 0;
+      for(unsigned int i=n; i>0; --i)
+        sum += *wPtr++ * *dPtr++;
+      return sum;
+    }
     UIntT quadLen = (n & ~0x3);
     const float* const ewPtr = wPtr + quadLen;
-    const float* dPtr = v2;
     __m128 sum = _mm_setzero_ps ();
     
     
@@ -132,14 +122,14 @@ namespace RavlBaseVectorN {
     return ret;
   }
 
-
   
-  static double SSEQuadProductD(const double *Data, const double *Weights1,
-                            const double *Weights2, size_t Size) {
-    const double* wPtr = Weights1;
-    const double* w2Ptr = Weights2;
-    const double* const ewPtr = Weights1 + (Size & ~0x1);
-    const double* dPtr = Data;
+  
+  static double SSEQuadProductD(const double *data, const double *weights1,
+                            const double *weights2, size_t size) {
+    const double* wPtr = weights1;
+    const double* w2Ptr = weights2;
+    const double* const ewPtr = weights1 + (size & ~0x1);
+    const double* dPtr = data;
     __m128d sum = _mm_setzero_pd();
     if(((((unsigned long int) dPtr) & 0xf) == 0) && ((((unsigned long int) wPtr) & 0xf) == 0)  && ((((unsigned long int) w2Ptr) & 0xf) == 0))  { // Data 16-byte aligned ?
       while(wPtr != ewPtr) {
@@ -168,7 +158,7 @@ namespace RavlBaseVectorN {
       }
     }
     
-    if(Size & 1) // Odd length ?
+    if(size & 1) // Odd length ?
       {
         const __m128d val = _mm_load_sd(dPtr++);
         sum = _mm_add_pd(sum,
