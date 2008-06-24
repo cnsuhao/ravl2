dfd75dbe3f86691ca1481a9a8b8e5f8513f99e6f
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2008-06-24 14:01:46 +0000

    Use RAVL standard naming.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6861 7226f50c-d813-0410-99bf-979b288ab488

--- Core/Base/VectorUtils.hh
+++ Core/Base/VectorUtils.hh
@@ -12,17 +12,17 @@ namespace RavlBaseVectorN {
   extern double (*g_DotProductD)(const double*, const double*, size_t) ;
   extern float (*g_DotProductF)(const float*, const float*, size_t) ;
   extern double (*g_QuadProductD)(const double*, const double*, const double*, size_t) ;
-
-  inline double DotProduct(const double* v1, const double* v2, size_t Size) 
-  { return (*g_DotProductD)(v1,v2,Size); }
+  
+  inline double DotProduct(const double* v1, const double* v2, size_t size) 
+  { return (*g_DotProductD)(v1,v2,size); }
   //: Fast double dot product, with optional hardware speedup
 
-  inline float DotProduct(const float* v1, const float* v2, size_t Size) 
-  { return (*g_DotProductF)(v1,v2,Size); }
+  inline float DotProduct(const float* v1, const float* v2, size_t size) 
+  { return (*g_DotProductF)(v1,v2,size); }
     //: Fast float dot product, with optional hardware speedup
 
-  inline double QuadProduct(const double* v1, const double* v2, const double* v3, size_t Size) 
-  { return (*g_QuadProductD)(v1,v2,v3,Size); }
+  inline double QuadProduct(const double* v1, const double* v2, const double* v3, size_t size) 
+  { return (*g_QuadProductD)(v1,v2,v3,size); }
   //: Fast double quadratic product, with optional hardware speedup
   // Performs: Sum over arrays of v1 * (v1 * v3 + v2)
 
