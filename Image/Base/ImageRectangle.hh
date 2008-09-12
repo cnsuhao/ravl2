4ffb9e0da22aa230adb38ae9a2b18db345486579
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2008-09-12 09:30:47 +0000

    Made AspectRatio() const
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl/main/Ravl@6941 7226f50c-d813-0410-99bf-979b288ab488

--- Image/Base/ImageRectangle.hh
+++ Image/Base/ImageRectangle.hh
@@ -106,7 +106,7 @@ namespace RavlImageN {
     //: Mirrors the image rectangle along its right edge.
     // Returns a reference to this rectangle.
     
-    RealT AspectRatio() {
+    RealT AspectRatio() const {
       return static_cast<RealT>(Cols())/static_cast<RealT>(Rows());
     }
     //: Calculate the aspect ratio of the image rectangle
